7ddcc3d6b5425feedbe820afe92316b916e66895
pelikhan <pelikhan@example.com>
2008-01-31 06:04:24 +0000

    fixed bug in vertexdistancerecorder
    git-svn-id: https://quickgraph.svn.codeplex.com/svn@15206 142e9c35-602f-4880-9f37-ec48a24daa6a

--- sources/QuickGraph/Algorithms/Observers/VertexDistanceRecorderObserver.cs
+++ sources/QuickGraph/Algorithms/Observers/VertexDistanceRecorderObserver.cs
@@ -39,8 +39,6 @@ namespace QuickGraph.Algorithms.Observers
             if (algorithm == null)
                 throw new ArgumentNullException("algorithm");
 
-            algorithm.InitializeVertex += new VertexEventHandler<TVertex>(this.InitializeVertex);
-            algorithm.DiscoverVertex += new VertexEventHandler<TVertex>(this.DiscoverVertex);
             algorithm.TreeEdge += new EdgeEventHandler<TVertex, TEdge>(this.TreeEdge);
         }
 
@@ -49,25 +47,15 @@ namespace QuickGraph.Algorithms.Observers
             if (algorithm == null)
                 throw new ArgumentNullException("arg");
 
-            algorithm.InitializeVertex -= new VertexEventHandler<TVertex>(this.InitializeVertex);
-            algorithm.DiscoverVertex -= new VertexEventHandler<TVertex>(this.DiscoverVertex);
             algorithm.TreeEdge -= new EdgeEventHandler<TVertex, TEdge>(this.TreeEdge);
-        }
-
-        private void InitializeVertex(Object sender, VertexEventArgs<TVertex> args)
-        {
-            this.distances[args.Vertex] = int.MaxValue;
-        }
-
-        private void DiscoverVertex(Object sender, VertexEventArgs<TVertex> args)
-        {
-            this.distances[args.Vertex] = 0;
         }
 
         private void TreeEdge(Object sender, EdgeEventArgs<TVertex,TEdge> args)
         {
-            this.distances[args.Edge.Target] = 
-                this.distances[args.Edge.Source] + 1;
+            int sourceDistance;
+            if(!this.distances.TryGetValue(args.Edge.Source, out sourceDistance))
+                this.distances[args.Edge.Source] = sourceDistance = 0;
+            this.distances[args.Edge.Target] = sourceDistance + 1;
         }
     }
 }
