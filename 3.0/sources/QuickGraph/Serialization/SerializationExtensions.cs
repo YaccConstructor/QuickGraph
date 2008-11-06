42e668ff579db7da08b408da1ba8b5fb61f6de35
pelikhan <pelikhan@example.com>
2008-11-06 06:14:32 +0000

    added serialization helpers
    git-svn-id: https://quickgraph.svn.codeplex.com/svn@23951 142e9c35-602f-4880-9f37-ec48a24daa6a

---
+++ 3.0/sources/QuickGraph/Serialization/SerializationExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Diagnostics.Contracts;
+
+namespace QuickGraph.Serialization
+{
+    public static class SerializationExtensions
+    {
+        public static void SerializeToBinary<TVertex, TEdge>(
+            this IGraph<TVertex, TEdge> graph,
+            Stream stream)
+            where TEdge : IEdge<TVertex>
+        {            
+            CodeContract.Requires(graph != null);
+            CodeContract.Requires(stream != null);
+            CodeContract.Requires(stream.CanWrite);
+
+            var formatter = new BinaryFormatter();
+            formatter.Serialize(stream, graph);
+        }
+
+        public static TGraph DeserializeFromBinary<TVertex, TEdge, TGraph>(this Stream stream)
+            where TGraph : IGraph<TVertex, TEdge>
+            where TEdge : IEdge<TVertex>
+        {
+            CodeContract.Requires(stream != null);
+            CodeContract.Requires(stream.CanRead);
+
+            var formatter = new BinaryFormatter();
+            var result = formatter.Deserialize(stream);
+            return (TGraph)result;
+        }
+
+    }
+}
