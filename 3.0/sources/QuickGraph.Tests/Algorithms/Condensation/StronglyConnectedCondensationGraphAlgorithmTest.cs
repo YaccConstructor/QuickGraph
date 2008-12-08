b984586ce2deecc0e6d8e57be6091ef405e2d54b
pelikhan <pelikhan@example.com>
2008-12-08 08:28:55 +0000

    test migration
    git-svn-id: https://quickgraph.svn.codeplex.com/svn@27449 142e9c35-602f-4880-9f37-ec48a24daa6a

--- 3.0/sources/QuickGraph.Tests/Algorithms/Condensation/StronglyConnectedCondensationGraphAlgorithmTest.cs
+++ 3.0/sources/QuickGraph.Tests/Algorithms/Condensation/StronglyConnectedCondensationGraphAlgorithmTest.cs
@@ -1,89 +1,88 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
-
-using QuickGraph.Unit;
-
 using QuickGraph.Algorithms.Condensation;
 using Microsoft.Pex.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace QuickGraph.Algorithms.Condensation
 {
-    [PexClass]
-    [TypeFixture(typeof(IMutableVertexAndEdgeListGraph<string, Edge<string>>))]
-    [TypeFactory(typeof(AdjacencyGraphFactory))]
-    [TypeFactory(typeof(BidirectionalGraphFactory))]
+    [TestClass, PexClass]
     public partial class StronglyConnectedCondensationGraphAlgorithmTest
     {
-        private CondensationGraphAlgorithm<string, Edge<string>,AdjacencyGraph<string,Edge<string>>> algo;
-        [Test, PexMethod]
+        [PexMethod]
         public void CondensateAndCheckVertexCount(
             [PexAssumeNotNull]IVertexAndEdgeListGraph<string, Edge<string>> g)
         {
-            this.algo = new CondensationGraphAlgorithm<string, Edge<string>, AdjacencyGraph<string, Edge<string>>>(g);
+            var algo = new CondensationGraphAlgorithm<string, Edge<string>, AdjacencyGraph<string, Edge<string>>>(g);
             algo.Compute();
-            CheckVertexCount(g);
+            CheckVertexCount(g, algo);
         }
 
-        [Test, PexMethod]
+        [PexMethod]
         public void CondensateAndCheckEdgeCount(
             [PexAssumeNotNull]IVertexAndEdgeListGraph<string, Edge<string>> g)
         {
-            this.algo = new CondensationGraphAlgorithm<string, Edge<string>, AdjacencyGraph<string, Edge<string>>>(g);
+            var algo = new CondensationGraphAlgorithm<string, Edge<string>, AdjacencyGraph<string, Edge<string>>>(g);
             algo.Compute();
-            CheckEdgeCount(g);
+            CheckEdgeCount(g, algo);
         }
-        [Test, PexMethod]
+        [PexMethod]
         public void CondensateAndCheckComponentCount(
             [PexAssumeNotNull]IVertexAndEdgeListGraph<string, Edge<string>> g)
         {
-            this.algo = new CondensationGraphAlgorithm<string, Edge<string>, AdjacencyGraph<string, Edge<string>>>(g);
+            var algo = new CondensationGraphAlgorithm<string, Edge<string>, AdjacencyGraph<string, Edge<string>>>(g);
             algo.Compute();
-            CheckComponentCount(g);
+            CheckComponentCount(g, algo);
         }
-        [Test, PexMethod]
+
+        [PexMethod]
         public void CondensateAndCheckDAG(
             [PexAssumeNotNull]IVertexAndEdgeListGraph<string, Edge<string>> g)
         {
-            this.algo = new CondensationGraphAlgorithm<string, Edge<string>, AdjacencyGraph<string, Edge<string>>>(g);
+            var algo = new CondensationGraphAlgorithm<string, Edge<string>, AdjacencyGraph<string, Edge<string>>>(g);
             algo.Compute();
-            CheckDAG(g);
+            CheckDAG(g, algo);
         }
 
         private void CheckVertexCount(
-            IVertexAndEdgeListGraph<string, Edge<string>> g)
+            IVertexAndEdgeListGraph<string, Edge<string>> g,
+            CondensationGraphAlgorithm<string, Edge<string>, AdjacencyGraph<string, Edge<string>>> algo)
         {
             int count = 0;
-            foreach (AdjacencyGraph<string,Edge<string>> vertices in this.algo.CondensatedGraph.Vertices)
+            foreach (AdjacencyGraph<string,Edge<string>> vertices in algo.CondensatedGraph.Vertices)
                 count += vertices.VertexCount;
             Assert.AreEqual(g.VertexCount, count, "VertexCount does not match");
         }
 
-        private void CheckEdgeCount(IVertexAndEdgeListGraph<string, Edge<string>> g)
+        private void CheckEdgeCount(IVertexAndEdgeListGraph<string, Edge<string>> g,
+            CondensationGraphAlgorithm<string, Edge<string>, AdjacencyGraph<string, Edge<string>>> algo)
         {
             // check edge count
             int count = 0;
-            foreach (CondensatedEdge<string, Edge<string>, AdjacencyGraph<string, Edge<string>>> edges in this.algo.CondensatedGraph.Edges)
+            foreach (CondensatedEdge<string, Edge<string>, AdjacencyGraph<string, Edge<string>>> edges in algo.CondensatedGraph.Edges)
                 count += edges.Edges.Count;
-            foreach (AdjacencyGraph<string, Edge<string>> vertices in this.algo.CondensatedGraph.Vertices)
+            foreach (AdjacencyGraph<string, Edge<string>> vertices in algo.CondensatedGraph.Vertices)
                 count += vertices.EdgeCount;
             Assert.AreEqual(g.EdgeCount, count, "EdgeCount does not match");
         }
 
 
-        private void CheckComponentCount(IVertexAndEdgeListGraph<string, Edge<string>> g)
+        private void CheckComponentCount(IVertexAndEdgeListGraph<string, Edge<string>> g,
+            CondensationGraphAlgorithm<string, Edge<string>, AdjacencyGraph<string, Edge<string>>> algo)
         {
             // check number of vertices = number of storngly connected components
             int components = g.StronglyConnectedComponents<string, Edge<string>>(new Dictionary<string, int>());
             Assert.AreEqual(components, algo.CondensatedGraph.VertexCount, "ComponentCount does not match");
         }
 
-        private void CheckDAG(IVertexAndEdgeListGraph<string, Edge<string>> g)
+        private void CheckDAG(IVertexAndEdgeListGraph<string, Edge<string>> g,
+            CondensationGraphAlgorithm<string, Edge<string>, AdjacencyGraph<string, Edge<string>>> algo)
         {
             // check it's a dag
             try
             {
-                this.algo.CondensatedGraph.TopologicalSort();
+                algo.CondensatedGraph.TopologicalSort();
             }
             catch (NonAcyclicGraphException)
             {
