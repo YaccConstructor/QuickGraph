f1344efecac2ab2b207d79bb597e7b446f3ee0bb
pelikhan <pelikhan@example.com>
2008-12-11 06:30:57 +0000

    looking at the k shortest path problem
    git-svn-id: https://quickgraph.svn.codeplex.com/svn@28033 142e9c35-602f-4880-9f37-ec48a24daa6a

---
+++ 3.0/sources/QuickGraph/Algorithms/RankedShortestPath/MartinPascoalSantosRankedShortestPath.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using QuickGraph.Algorithms.Services;
+
+namespace QuickGraph.Algorithms.RankedShortestPath
+{
+    /// <summary>
+    /// under construction
+    /// </summary>
+    /// <typeparam name="TVertex"></typeparam>
+    /// <typeparam name="TEdge"></typeparam>
+    class MartinPascoalSantosRankedShortestPath<TVertex, TEdge>
+        : RankingShortestPathBase<TVertex, TEdge, IVertexListGraph<TVertex, TEdge>>
+        where TEdge : IEdge<TVertex>
+    {
+        public MartinPascoalSantosRankedShortestPath(IAlgorithmComponent host, IVertexListGraph<TVertex, TEdge> visitedGraph)
+            : base(host, visitedGraph)
+        { }
+
+        protected override void InternalCompute()
+        {
+            TVertex root;
+            if (!this.TryGetRootVertex(out root))
+                throw new InvalidOperationException("root vertex not set");
+
+            var h = new Dictionary<int, TVertex>();
+            var h1 = new Dictionary<TVertex, int>();
+            var count = new Dictionary<TVertex, int>();
+            foreach (var v in this.VisitedGraph.Vertices)
+                count[v] = 0;
+            int elm = 1;
+            var X = new Dictionary<int, int>();
+
+            h[elm] = root;
+            h1[root] = elm;
+            X.Add(elm, elm);
+
+            while (X.Count > 0)
+            {
+                var k = X.First().Key;
+                X.Remove(k);
+                var i = h[k];
+                foreach (var e in this.VisitedGraph.OutEdges(i))
+                {
+                    var j = e.Target;
+                    if (count[j] == K)
+                    {
+                    }
+                    else
+                    {
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ 3.0/sources/QuickGraph/Algorithms/RankedShortestPath/RankedShortestPathBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using QuickGraph.Algorithms.Services;
+
+namespace QuickGraph.Algorithms.RankedShortestPath
+{
+    abstract class RankingShortestPathBase<TVertex, TEdge, TGraph>
+        : RootedAlgorithmBase<TVertex, TGraph>
+        where TEdge : IEdge<TVertex>
+        where TGraph : IGraph<TVertex, TEdge>
+    {
+        public int K { get; set; }
+
+        protected RankingShortestPathBase(IAlgorithmComponent host, TGraph visitedGraph)
+            : base(host, visitedGraph)
+        { }
+    }
+}
