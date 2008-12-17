8f5508c7df003a91b00989d663b2d3f89cd8de87
pelikhan <pelikhan@example.com>
2008-12-17 14:22:13 +0000

    playing nicer with contracts
    git-svn-id: https://quickgraph.svn.codeplex.com/svn@28704 142e9c35-602f-4880-9f37-ec48a24daa6a

---
+++ 3.0/sources/QuickGraph.Tests/AssemblyContextTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics.Contracts;
+
+namespace QuickGraph.Tests
+{
+    [TestClass]
+    public class AssemblyContextTest
+    {
+        [AssemblyInitialize]
+        public static void Initialize(TestContext ctx)
+        {
+            // avoid contract violation kill the process
+            Contract.ContractFailed += new EventHandler<ContractFailedEventArgs>(Contract_ContractFailed);
+        }
+
+        static void Contract_ContractFailed(object sender, System.Diagnostics.Contracts.ContractFailedEventArgs e)
+        {
+            e.Handled = true;
+            Assert.Fail("{0}: {1} {2}", e.FailureKind, e.DebugMessage, e.Condition);
+        }
+    }
+}
