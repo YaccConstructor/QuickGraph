63bd144edfbf486824b2c09cf58f2f44fcef17ef
pelikhan <pelikhan@example.com>
2009-04-06 05:51:02 +0000

    fixed tests
    git-svn-id: https://quickgraph.svn.codeplex.com/svn@32843 142e9c35-602f-4880-9f37-ec48a24daa6a

--- 3.0/sources/QuickGraph.Tests/Properties/PexAssemblyInfo.cs
+++ 3.0/sources/QuickGraph.Tests/Properties/PexAssemblyInfo.cs
@@ -15,4 +15,5 @@ using QuickGraph;
     AcceptExceptionSubtypes = true)]
 
 [assembly: PexAllowedContractRequiresFailureAtTypeUnderTestSurface]
-[assembly:PexGenericArguments(typeof(int), typeof(Edge<int>))]
+[assembly: PexGenericArguments(typeof(int), typeof(Edge<int>))]
+[assembly: PexGenericArguments(typeof(int), typeof(SEdge<int>))]
