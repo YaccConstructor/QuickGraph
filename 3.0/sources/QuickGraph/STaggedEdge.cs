a2934886c004a354578d8981c41219fdee180d19
pelikhan <pelikhan@example.com>
2009-04-25 13:45:10 +0000

    added STaggedEdge
    git-svn-id: https://quickgraph.svn.codeplex.com/svn@33500 142e9c35-602f-4880-9f37-ec48a24daa6a

---
+++ 3.0/sources/QuickGraph/STaggedEdge.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Runtime.InteropServices;
+
+namespace QuickGraph
+{
+    /// <summary>
+    /// A tagged edge as value type.
+    /// </summary>
+    /// <typeparam name="TVertex"></typeparam>
+    /// <typeparam name="TTag"></typeparam>
+#if !SILVERLIGHT
+    [Serializable]
+#endif
+    [StructLayout(LayoutKind.Auto)]
+    public struct STaggedEdge<TVertex, TTag>
+        : IEdge<TVertex>
+    {
+        readonly TVertex source;
+        readonly TVertex target;
+        TTag tag;
+
+        public STaggedEdge(TVertex source, TVertex target, TTag tag)
+        {
+            Contract.Requires(source != null);
+            Contract.Requires(target != null);
+
+            this.source = source;
+            this.target = target;
+            this.tag = tag;
+            this.TagChanged = null;
+        }
+
+        public TVertex Source
+        {
+            get { return this.source; }
+        }
+
+        public TVertex Target
+        {
+            get { return this.target; }
+        }
+
+        public event EventHandler TagChanged;
+
+        void OnTagChanged(EventArgs e)
+        {
+            var eh = this.TagChanged;
+            if (eh != null)
+                eh(this, e);
+        }
+
+        public TTag Tag
+        {
+            get { return this.tag; }
+            set
+            {
+                if (!object.Equals(this.tag, value))
+                {
+                    this.tag = value;
+                    this.OnTagChanged(EventArgs.Empty);
+                }
+            }
+        }
+    }
+}
