145c5a088623bdcb9b679b05c133f45371167711
pelikhan <pelikhan@example.com>
2009-04-26 14:27:30 +0000

    added taggedequatableedge (class/struct)
    git-svn-id: https://quickgraph.svn.codeplex.com/svn@33522 142e9c35-602f-4880-9f37-ec48a24daa6a

--- 3.0/sources/QuickGraph/TaggedEquatableEdge.cs
+++ 3.0/sources/QuickGraph/TaggedEquatableEdge.cs
@@ -1,52 +1,35 @@
 ﻿using System;
 using System.Diagnostics.Contracts;
-using System.Runtime.InteropServices;
 using System.Diagnostics;
 
 namespace QuickGraph
 {
     /// <summary>
-    /// A tagged edge as value type.
+    /// An equatable, tagged, edge
     /// </summary>
     /// <typeparam name="TVertex"></typeparam>
     /// <typeparam name="TTag"></typeparam>
 #if !SILVERLIGHT
     [Serializable]
 #endif
-    [StructLayout(LayoutKind.Auto)]
     [DebuggerDisplay("{Source}->{Target}:{Tag}")]
-    public struct STaggedEdge<TVertex, TTag>
-        : IEdge<TVertex>
+    public class TaggedEquatableEdge<TVertex, TTag>
+        : EquatableEdge<TVertex>
         , ITagged<TTag>
     {
-        readonly TVertex source;
-        readonly TVertex target;
-        TTag tag;
+        private TTag tag;
 
-        public STaggedEdge(TVertex source, TVertex target, TTag tag)
+        public TaggedEquatableEdge(TVertex source, TVertex target, TTag tag)
+            : base(source, target)
         {
-            Contract.Requires(source != null);
-            Contract.Requires(target != null);
+            Contract.Ensures(Object.Equals(this.Tag, tag));
 
-            this.source = source;
-            this.target = target;
             this.tag = tag;
-            this.TagChanged = null;
-        }
-
-        public TVertex Source
-        {
-            get { return this.source; }
-        }
-
-        public TVertex Target
-        {
-            get { return this.target; }
         }
 
         public event EventHandler TagChanged;
 
-        void OnTagChanged(EventArgs e)
+        protected virtual void OnTagChanged(EventArgs e)
         {
             var eh = this.TagChanged;
             if (eh != null)
