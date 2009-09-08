1e7d513c31dc69e78d36cc58275a5931f12c8308
pelikhan <pelikhan@example.com>
2009-09-08 03:58:24 +0000

    updated perf session
    git-svn-id: https://quickgraph.svn.codeplex.com/svn@36557 142e9c35-602f-4880-9f37-ec48a24daa6a

--- 3.0/sources/QuickGraph.Perf/Program.cs
+++ 3.0/sources/QuickGraph.Perf/Program.cs
@@ -8,6 +8,8 @@ using QuickGraph.Tests.Algorithms.MinimumSpanningTree;
 using QuickGraph.Serialization;
 using QuickGraph.Algorithms.Observers;
 using QuickGraph.Collections;
+using QuickGraph.Algorithms;
+using QuickGraph.Algorithms.Search;
 
 namespace QuickGraph.Perf
 {
@@ -18,15 +20,15 @@ namespace QuickGraph.Perf
             // new TarjanOfflineLeastCommonAncestorAlgorithmTest().TarjanOfflineLeastCommonAncestorAlgorithmAll();
             // new DijkstraShortestPathAlgorithmTest().DijkstraAll();
             // new MinimumSpanningTreeTest().PrimKruskalMinimumSpanningTreeAll();
-            var g = TestGraphFactory.LoadGraph(@"graphml\repro12359.graphml");
+            var g = TestGraphFactory.LoadBidirectionalGraph(@"graphml\repro12359.graphml");
             var distances = new Dictionary<Edge<string>, double>(g.EdgeCount);
             foreach (var e in g.Edges)
                 distances[e] = g.OutDegree(e.Source) + 1;
             int i = 0;
+            var root = Enumerable.First(g.Vertices);
             foreach (var v in g.Vertices)
             {
-                if (i++ > 5) break;
-                Dijkstra(g, distances, v);
+                FrontierDijkstra(g, distances, root, v);
             }
         }
 
@@ -38,11 +40,29 @@ namespace QuickGraph.Perf
         {
             var algo = new DijkstraShortestPathAlgorithm<TVertex, TEdge>(
                 g,
-                e => distances[e]
+                AlgorithmExtensions.GetIndexer(distances)
                 );
             var predecessors = new VertexPredecessorRecorderObserver<TVertex, TEdge>();
             using (predecessors.Attach(algo))
                 algo.Compute(root);
+        }
+
+        static void FrontierDijkstra<TVertex, TEdge>(
+            IBidirectionalGraph<TVertex, TEdge> g,
+            Dictionary<TEdge, double> distances,
+            TVertex root,
+            TVertex target)
+            where TEdge : IEdge<TVertex>
+        {
+            var algo = new BestFirstFrontierSearchAlgorithm<TVertex, TEdge>(
+                null,
+                g,
+                AlgorithmExtensions.GetIndexer(distances),
+                DistanceRelaxers.ShortestDistance
+                );
+            var predecessors = new VertexPredecessorRecorderObserver<TVertex, TEdge>();
+            using (predecessors.Attach(algo))
+                algo.Compute(root, target);
         }
     }
 }
