1c5f03ec5919ec50928c8e30921ab0e3592b23c1
Rhishikesh Limaye <rhishi@gmail.com>
2010-09-09 13:53:08 -0700

    Added EquateGraphs that contains a simple equality test for two graphs.

---
+++ 3.0/sources/QuickGraph/EquateGraphs.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuickGraph
+{
+    public class EquateGraphs
+    {
+        public static bool Equate<V, E>(IVertexAndEdgeListGraph<V, E> g, IVertexAndEdgeListGraph<V, E> h)
+            where E : IEdge<V>
+        {
+            if (ReferenceEquals(g, null))
+                return ReferenceEquals(h, null);
+            if (ReferenceEquals(h, null))
+                return ReferenceEquals(g, null);
+
+            if (ReferenceEquals(g, h))
+                return true;
+
+            if (g.VertexCount != h.VertexCount)
+                return false;
+
+            if (g.EdgeCount != h.EdgeCount)
+                return false;
+
+            foreach (var v in g.Vertices)
+            {
+                if (!h.ContainsVertex(v))
+                    return false;
+            }
+
+            foreach (var e in g.Edges)
+            {
+                if (!h.ContainsEdge(e))
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
