a03c3ad355b225d7e4e7657dfa43957e90e44b86
Rhishikesh Limaye <rhishi@gmail.com>
2010-09-08 19:13:53 -0700

    Added a simple test for SourceFirstTopologicalSortAlgorithm that induces a very easy failure.

--- 3.0/sources/QuickGraph.Tests/Algorithms/SourceFirstTopologicalSortAlgorithmTest.cs
+++ 3.0/sources/QuickGraph.Tests/Algorithms/SourceFirstTopologicalSortAlgorithmTest.cs
@@ -28,5 +28,22 @@ namespace QuickGraph.Algorithms
             catch (NonAcyclicGraphException)
             { }
         }
+
+        [TestMethod]
+        public void SortAnotherOne()
+        {
+            var g = new BidirectionalGraph<int, Edge<int>>();
+
+            g.AddVertexRange(new int[5] { 0, 1, 2, 3, 4 });
+            g.AddEdge(new Edge<int>(0, 1));
+            g.AddEdge(new Edge<int>(1, 2));
+            g.AddEdge(new Edge<int>(1, 3));
+            g.AddEdge(new Edge<int>(2, 3));
+            g.AddEdge(new Edge<int>(3, 4));
+
+            var topo = new SourceFirstTopologicalSortAlgorithm<int, Edge<int>>(g);
+            topo.Compute();
+        }
+
     }
 }
