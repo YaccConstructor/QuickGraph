8dff3bd5841f33121430c7b561aa96b5bf00ae89
gsv <rsdpisuy@gmail.com>
2013-11-19 12:47:16 +0400

    Example of simple  semantic.

--- DotParserProject/DotParserProject/Program.fs
+++ DotParserProject/DotParserProject/Program.fs
@@ -10,8 +10,7 @@ open Yard.Generators.RNGLR.AST
 
 open DotParserProject.DotParser
 
-let src = "..\\..\\..\\test_inputs\\test2.dot"
-
+let src = "..\\..\\..\\test_inputs\\test3.dot"
 let tokens = 
     let lexbuf = Lexing.LexBuffer<_>.FromTextReader <| new System.IO.StreamReader(src)
     seq { while not lexbuf.IsPastEndOfStream do
@@ -21,14 +20,18 @@ match buildAst tokens with
 | Error (pos, token, msg, debugFuns, _) ->
     printfn "Error on position %d, token %A: %s" pos token msg
 | Success (ast, errors) ->
-    ast.PrintAst()
-//    let args = {
-//        tokenToRange = fun _ -> Unchecked.defaultof<_>, Unchecked.defaultof<_>
-//        zeroPosition = Unchecked.defaultof<_>
-//        clearAST = false
-//        filterEpsilons = false
-//    }
-//    let result:List<double> = translate args ast errors
+    //ast.PrintAst()
+    let args = {
+        tokenToRange = fun _ -> Unchecked.defaultof<_>, Unchecked.defaultof<_>
+        zeroPosition = Unchecked.defaultof<_>
+        clearAST = false
+        filterEpsilons = false
+    }
+    
+    defaultAstToDot ast "ast.dot"
+    let result:list<list<string>> = translate args ast errors
+    vrt |> ResizeArray.iter (printfn "vrt = %A;")
+    printfn "result = %A" result
     //defaultAstToDot ast @"..\..\astFromSeq.dot"
 //    defaultAstToDot ast @"..\..\astFromDot.dot"
 //    printfn "%A" result
