237f4ef00a9de9502bf44423d43d57d6ed9bdc52
MaratX <Marat@Marat-PC>
2013-12-15 18:48:25 +0400

    Interface for users has been created

--- DotParserProject/DotParserProject/GraphDataContainer.fs
+++ DotParserProject/DotParserProject/GraphDataContainer.fs
@@ -2,6 +2,7 @@
 
 open System.Collections.Generic
 open System.IO
+open Option
 
 type GraphDataContainer = class
     val private vertices_lists : ResizeArray<string list>
@@ -20,6 +21,7 @@ type GraphDataContainer = class
         assign_stmt_list = new ResizeArray<string*string>()
         }
 
+    // methods for collecting graph's data
     member x.AddGeneralInfo (info_list: list<string*string>) =
         let new_info = info_list |> List.filter (fun (key,value) -> not (x.graph_info.ContainsKey key))
         new_info |> List.map x.graph_info.Add |> ignore
@@ -38,12 +40,35 @@ type GraphDataContainer = class
             x.vertices_lists.Add elems_list
         if not attr_list.IsEmpty then
             x.AddAttrToDict (elems_list.ToString()) attr_list x.vertices_attrs
-    
+
     member x.AddAssignStmt key value =
         x.assign_stmt_list.Add (key, value) |> ignore 
 
+    //methods to get collected data
     member x.GraphName = x.graph_info.Item "name"
+    member x.IsStrict = (x.graph_info.Item "is_strict").Equals "strict"
+    member x.Type = x.graph_info.Item "type"
+    member x.GraphAttributes = x.general_attrs.Item "graph"
+    member x.NodeAttributes = x.general_attrs.Item "node"
+    member x.EdgeAttributes = x.general_attrs.Item "edge"
 
+    member x.VerticeTags vertice_name =
+        match x.vertices_attrs.ContainsKey vertice_name with
+        | true -> x.vertices_attrs.Item vertice_name
+        | false -> []
+
+    member x.AssignStatements =
+        x.assign_stmt_list.ToArray()
+
+    member x.GetEdgeArray handler =
+        let WalkWithHandler lst = x.WalkEdges handler lst [||]
+        x.Walk WalkWithHandler
+
+    member x.GetTaggedEdgeArray handler =
+        let WalkWithHandler lst = x.WalkEdgesAndTags handler lst [||]
+        x.Walk WalkWithHandler
+
+    //private section
     member private x.AddAttrToDict key value (attrs_list:Dictionary<string, (string*string) list>) =
         let contains_node = attrs_list.ContainsKey key
         if not contains_node then 
@@ -51,6 +76,43 @@ type GraphDataContainer = class
         else
             attrs_list.[key] <- List.concat [attrs_list.[key]; value]
 
+    member private x.WalkEdges handler (lst: string list) edges =
+        match lst with
+        | first::second::rest ->
+            let edges = Array.append edges [|(handler first second)|]
+            x.WalkEdges handler (second::rest) edges
+        | head::rest -> edges
+        | [] -> edges
+    
+    member private x.WalkEdgesAndTags handler (lst: string list) edges =
+        match lst with
+        | first::second::rest ->
+            let edge = ResizeArray.find (fun list -> x.FindEdge first second list) x.vertices_lists
+            let str_edge = edge.ToString()
+            match x.edges_attrs.ContainsKey str_edge with
+            | true ->
+                let edges = Array.append edges [|(handler first second (x.edges_attrs.Item str_edge))|]
+                x.WalkEdgesAndTags handler (second::rest) edges
+            | false ->
+                let edges = Array.append edges [|(handler first second [])|]
+                x.WalkEdgesAndTags handler (second::rest) edges
+        | head::rest -> edges
+        | [] -> edges
+
+    member private x.Walk handler =
+        let res = ResizeArray.map handler x.vertices_lists
+        let res_seq = seq {for i in 0 .. (ResizeArray.length res) - 1 do
+                                yield res.Item i}
+        Array.concat res_seq
+
+    member private x.FindEdge (v1: string) (v2: string) (vert_lists : string list) =
+        let res1 = List.tryFind v1.Equals vert_lists
+        let res2 = List.tryFind v2.Equals vert_lists
+        match (isSome res1) && (isSome res2) with
+        | true -> true
+        | false -> false
+    
+    // methods to debug)
     member x.ToFiles() =
         let f_general_info = "..\\..\\..\\test_output\\collected_data_gen_info.txt"
         let f_vert = "..\\..\\..\\test_output\\collected_data_vertices.txt"
--- DotParserProject/DotParserProject/Program.fs
+++ DotParserProject/DotParserProject/Program.fs
@@ -9,11 +9,10 @@ open Yard.Generators.RNGLR.Parser
 open Yard.Generators.RNGLR.AST
 
 open DotParserProject.DotParser
-open DotParserProject.ParsingFuncs
 open System.Collections.Generic
 open QuickGraph
 
-let src = "..\\..\\..\\test_inputs\\test4.dot"
+let src = "..\\..\\..\\test_inputs\\test1.dot"
 let tokens = 
     let lexbuf = Lexing.LexBuffer<_>.FromTextReader <| new System.IO.StreamReader(src)
     seq { while not lexbuf.IsPastEndOfStream do
@@ -32,37 +31,51 @@ match buildAst tokens with
     }
     
     defaultAstToDot ast "ast.dot"
-//    let result:list<list<string>> = translate args ast errors
-//    printfn "result = %A" result
 //    defaultAstToDot ast @"..\..\astFromSeq.dot"
 //    defaultAstToDot ast @"..\..\astFromDot.dot"
 //    printfn "%A" result
     
     translate args ast errors |> ignore
 
-    let g = new AdjacencyGraph<string, SEdge<string>>()
-//    printfn ""
-//    for v in g.Vertices do
-//        printf "%A " v
-//    printfn ""
-//    for e in g.Edges do
-//        printfn "%A" e
-//    printfn ""
-    
-//    let quickgraph_vert = "..\\..\\..\\test_output\\quickgraph_vert.txt"
-//    let quickgraph_edges = "..\\..\\..\\test_output\\quickgraph_edges.txt"
-//    let outp = File.CreateText quickgraph_vert
-//    let outp2 = File.CreateText quickgraph_edges 
-//    for v in g.Vertices do
-//        outp.WriteLine v
-//    for e in g.Edges do
-//        outp2.WriteLine e
-//    outp.Close()
-//    outp2.Close()
     graphs.[0].ToFiles()
     for gr in graphs do
         gr.PrintAllCollectedData()
         printfn "---------------------------------------------------------"
     printfn "Completed"
-    
+
+    let handler1 v1 v2 =
+        new SUndirectedEdge<string>(v1,v2)
+
+    let handler2 v1 v2 =
+        let iv1 = Int32.Parse(v1)
+        let iv2 = Int32.Parse(v2)
+        new SUndirectedEdge<int>(iv1,iv2)
+
+    let handler3 v1 v2 (tags: list<string*string>) =
+        match tags.Length with
+        | 0 ->
+            new STaggedEdge<string, string>(v1, v2, "_no_tag_")
+        | _ ->
+            let _, tag = tags.[0]
+            new STaggedEdge<string, string>(v1, v2, tag)
+
+    let edges_array1 = graphs.[0].GetEdgeArray handler1
+    let gr1 = edges_array1.ToAdjacencyGraph<string, SUndirectedEdge<string>>()
+    for v in gr1.Vertices do
+        printf "%A " v
+    printfn ""
+    for e in gr1.Edges do
+        printfn "%A" e
+    printfn ""
+
+    let edges_array2 = graphs.[0].GetTaggedEdgeArray handler3
+    let gr2 = edges_array2.ToAdjacencyGraph<string, STaggedEdge<string, string>>()
+    for v in gr2.Vertices do
+        printf "%A " v
+    printfn ""
+    for e in gr2.Edges do
+        printfn "%A" e
+    printfn ""
+
+        
 let key = Console.ReadKey(true)
