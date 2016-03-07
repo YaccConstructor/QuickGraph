c7161bd336c1aefc0d412dd273a7e6d54b7cf737
Vlad Evtushenko <cryptically@yandex.ru>
2016-03-07 17:27:07 +0300

    Разобрался с кратчайшим путем, теперь надо понять как сделать неориентированный граф.

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using QuickGraph;
-using QuickGraph.Algorithms;
 using QuickGraph.Algorithms.Observers;
 using QuickGraph.Algorithms.ShortestPath;
 
@@ -10,32 +10,45 @@ namespace ConsoleApplication1
   {
     private static void Main()
     {
-      // получили Дейсктру для картинки из википедии
-      var dijkstra = GetSampleDijkstraGraph();
-      var predecessors = new VertexPredecessorRecorderObserver<char, Edge<char>>();
-      using (predecessors.Attach(dijkstra));
-        dijkstra.Compute('1');
+      // нашли кратчайший путь для стартовой вершины
+      var dij = GetDijkstraSampleGraph();
+      var vis = new VertexPredecessorRecorderObserver<char, Edge<char>>();
+      using (vis.Attach(dij))
+        dij.Compute('1');
 
-      // проблема в том, что у меня нет predecessors
+      var m = dij.VisitedGraph;
+      Console.WriteLine(m.GetType());
+
+      // первый самый короткий путь
       IEnumerable<Edge<char>> path;
-      predecessors.TryGetPath('1', out path);
-      //var col = path.ToList();
+      vis.TryGetPath('5', out path);
 
-      //Console.WriteLine(dijkstra.Distances['1']);
-      //Console.WriteLine(dijkstra.Distances['2']);
-      //Console.WriteLine(dijkstra.Distances['3']);
-      //Console.WriteLine(dijkstra.Distances['4']);
-      //Console.WriteLine(dijkstra.Distances['5']);
-      //Console.WriteLine(dijkstra.Distances['6']);
+      // хотим использовать Йена для 1ой и 5ой вершины и k = 3;
+      var k = 3;
+      var listShortestWays = new List<IEnumerable<Edge<char>>>();
+      var shortestWay = path;
+      var graph = dij.VisitedGraph; // изначальный граф с картинки
+
+      for (var i = 0; i < k - 1; i++)
+      {
+        var min = int.MaxValue;
+        IEnumerable<Edge<char>> newPath; // мб инициализировать?
+        // запомним версию графа без какого-то ребра
+        AdjacencyGraph<char, Edge<char>> graphSlot;
+        foreach (var edge in shortestWay)
+        {
+          
+        }
+      }
+
     }
 
-    private static DijkstraShortestPathAlgorithm<char, Edge<char>> GetSampleDijkstraGraph()
+    private static DijkstraShortestPathAlgorithm<char, Edge<char>> GetDijkstraSampleGraph()
     {
-      var g = new AdjacencyGraph<char, Edge<char>>();
+      var g = new AdjacencyGraph<char, Edge<char>>(true);
       var distances = new Dictionary<Edge<char>, double>();
-
-      // пример графа в википедии
       g.AddVertexRange("123456");
+
       AddEdge(g, distances, '1', '2', 7);
       AddEdge(g, distances, '1', '3', 9);
       AddEdge(g, distances, '1', '6', 14);
@@ -56,7 +69,7 @@ namespace ConsoleApplication1
       AddEdge(g, distances, '5', '4', 6);
       AddEdge(g, distances, '6', '5', 9);
 
-      return new DijkstraShortestPathAlgorithm<char, Edge<char>>(g, AlgorithmExtensions.GetIndexer(distances));
+      return new DijkstraShortestPathAlgorithm<char, Edge<char>>(g, e => distances[e]);
     }
 
     private static void AddEdge(
