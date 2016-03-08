da9a896f08d2e7e8e05a47edc364d734e3707689
Vlad Evtushenko <cryptically@yandex.ru>
2016-03-08 11:49:07 +0300

    - Переписано для Дейсктры

---
+++ ConsoleApplication1/InputModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using QuickGraph;
+
+namespace ConsoleApplication1
+{
+  public class InputModel
+  {
+    public AdjacencyGraph<char, Edge<char>> Graph { get; set; }
+    public Dictionary<Edge<char>, double> Distances { get; set; }
+  }
+}
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,83 +1,138 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using QuickGraph;
 using QuickGraph.Algorithms.Observers;
 using QuickGraph.Algorithms.ShortestPath;
+
+/*
+Todo:
+
+*/
 
 namespace ConsoleApplication1
 {
   internal static class Program
   {
+    // это нам дано на входе
+    private static char sourceVertix = '1';
+    private static char targetVertix = '5';
+
     private static void Main()
     {
-      // нашли кратчайший путь для стартовой вершины
-      var dij = GetDijkstraSampleGraph();
-      var vis = new VertexPredecessorRecorderObserver<char, Edge<char>>();
-      using (vis.Attach(dij))
-        dij.Compute('1');
 
-      var m = dij.VisitedGraph;
-      Console.WriteLine(m.GetType());
+      // вход будет в таком виде + целевые вершины + k:
+      var input = GenerateInput();
+      var k = 100;
 
-      // первый самый короткий путь
-      IEnumerable<Edge<char>> path;
-      vis.TryGetPath('5', out path);
-
-      // хотим использовать Йена для 1ой и 5ой вершины и k = 3;
-      var k = 3;
       var listShortestWays = new List<IEnumerable<Edge<char>>>();
-      var shortestWay = path;
-      var graph = dij.VisitedGraph; // изначальный граф с картинки
+      // нашли первый самый короткий путь
+      var shortestWay = GetShortestPathFromInput(input);
+      listShortestWays.Add(shortestWay);
 
       for (var i = 0; i < k - 1; i++)
       {
-        var min = int.MaxValue;
-        IEnumerable<Edge<char>> newPath; // мб инициализировать?
+        var minDistance = double.MaxValue;
+        IEnumerable<Edge<char>> pathSlot = null;
         // запомним версию графа без какого-то ребра
-        AdjacencyGraph<char, Edge<char>> graphSlot;
+        InputModel inputSlot = null;
         foreach (var edge in shortestWay)
         {
-          
-        }
-      }
+          // новые параметры для алгоритма Дейскстры: взяли старые и выкинули ребро
+          var newInput = RemoveEdge(input, edge);
 
+          //Найти кратчайший путь в перестроенном графе.
+          var newPath = GetShortestPathFromInput(newInput);
+          if (newPath == null)
+          {
+            continue;
+          }
+          var pathWeight = GetPathDistance(newPath, newInput);
+          if (pathWeight >= minDistance)
+          {
+            continue;
+          }
+          minDistance = pathWeight;
+          pathSlot = newPath;
+          inputSlot = newInput;
+        }
+        if (pathSlot == null)
+        {
+          break;
+        }
+        listShortestWays.Add(pathSlot);
+        shortestWay = pathSlot;
+        input = inputSlot;
+      }
     }
 
-    private static DijkstraShortestPathAlgorithm<char, Edge<char>> GetDijkstraSampleGraph()
+    private static double GetPathDistance(IEnumerable<Edge<char>> edges, InputModel input) =>
+      edges.Sum(edge => input.Distances[edge]);
+
+    private static IEnumerable<Edge<char>> GetShortestPathFromInput(InputModel input)
+    {
+      // нашли кратчайший путь для стартовой вершины
+      var dij = new DijkstraShortestPathAlgorithm<char, Edge<char>>(input.Graph, e => input.Distances[e]);
+      var vis = new VertexPredecessorRecorderObserver<char, Edge<char>>();
+      using (vis.Attach(dij))
+        dij.Compute(sourceVertix);
+
+      // первый самый короткий путь
+      IEnumerable<Edge<char>> path;
+
+      return vis.TryGetPath(targetVertix, out path) ? path : null;
+    }
+
+    private static InputModel RemoveEdge(InputModel old, Edge<char> edgeRemoving)
+    {
+      /*
+      Создаем новый граф и расстония на основе старого, без одного ребра
+      */
+      var newGraph = new AdjacencyGraph<char, Edge<char>>(true);
+      var newDistances = new Dictionary<Edge<char>, double>();
+      newGraph.AddVertexRange(old.Graph.Vertices);
+      foreach (var edge in old.Graph.Edges.Where(x => x.Source != edgeRemoving.Source ||
+                                                 x.Target != edgeRemoving.Target))
+      {
+        AddEdgeWithDistance(newGraph, newDistances, edge.Source, edge.Target, old.Distances[edge]);
+      }
+      return new InputModel
+      {
+        Distances = newDistances,
+        Graph = newGraph
+      };
+    } 
+
+    private static InputModel GenerateInput()
     {
       var g = new AdjacencyGraph<char, Edge<char>>(true);
       var distances = new Dictionary<Edge<char>, double>();
       g.AddVertexRange("123456");
 
-      AddEdge(g, distances, '1', '2', 7);
-      AddEdge(g, distances, '1', '3', 9);
-      AddEdge(g, distances, '1', '6', 14);
-      AddEdge(g, distances, '2', '3', 10);
-      AddEdge(g, distances, '2', '4', 15);
-      AddEdge(g, distances, '3', '4', 11);
-      AddEdge(g, distances, '3', '6', 2);
-      AddEdge(g, distances, '4', '5', 6);
-      AddEdge(g, distances, '5', '6', 9);
+      AddEdgeWithDistance(g, distances, '1', '2', 7);
+      AddEdgeWithDistance(g, distances, '1', '3', 9);
+      AddEdgeWithDistance(g, distances, '1', '6', 14);
+      AddEdgeWithDistance(g, distances, '2', '3', 10);
+      AddEdgeWithDistance(g, distances, '2', '4', 15);
+      AddEdgeWithDistance(g, distances, '3', '4', 11);
+      AddEdgeWithDistance(g, distances, '3', '6', 2);
+      AddEdgeWithDistance(g, distances, '4', '5', 6);
+      AddEdgeWithDistance(g, distances, '5', '6', 9);
 
-      AddEdge(g, distances, '2', '1', 7);
-      AddEdge(g, distances, '3', '1', 9);
-      AddEdge(g, distances, '6', '1', 14);
-      AddEdge(g, distances, '3', '2', 10);
-      AddEdge(g, distances, '4', '2', 15);
-      AddEdge(g, distances, '4', '3', 11);
-      AddEdge(g, distances, '6', '3', 2);
-      AddEdge(g, distances, '5', '4', 6);
-      AddEdge(g, distances, '6', '5', 9);
-
-      return new DijkstraShortestPathAlgorithm<char, Edge<char>>(g, e => distances[e]);
+      return new InputModel
+      {
+        Distances = distances,
+        Graph = g
+      };
     }
 
-    private static void AddEdge(
+    private static void AddEdgeWithDistance(
            AdjacencyGraph<char, Edge<char>> g,
            Dictionary<Edge<char>, double> distances,
            char source, char target, double weight)
     {
-      var ac = new Edge<char>(source, target); distances[ac] = weight; g.AddEdge(ac);
+      var ac = new Edge<char>(source, target);
+      distances[ac] = weight;
+      g.AddEdge(ac);
     }
   }
 }
