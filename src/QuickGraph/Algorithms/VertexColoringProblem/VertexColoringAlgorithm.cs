b203dbfcd12b816cafdfef3f275d76fa503d7213
Artyom <temic_m@mail.ru>
2016-04-08 19:49:21 +0300

    VertexColoringProblem + Tests

---
+++ src/QuickGraph/Algorithms/VertexColoringProblem/VertexColoringAlgorithm.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickGraph.Algorithms.GraphColoring.VertexColoring
+{
+    public sealed class InputModel<TVertex, TEdge> where TEdge : IEdge<TVertex>
+    {
+        public UndirectedGraph<TVertex, TEdge> Graph { get; set; }
+        public Dictionary<TVertex, int> Colors { get; set; }
+    }
+
+    public class VertexColoringAlgorithm<TVertex, TEdge> where TEdge : IEdge<TVertex>
+    {
+        private InputModel<TVertex, TEdge> input;
+
+        public VertexColoringAlgorithm(InputModel<TVertex, TEdge> input)
+        {
+            this.input = input;
+        }
+
+        public InputModel<TVertex, TEdge> Compute()
+        {
+            int V = input.Graph.VertexCount;
+            var listOfVertex = new List<IEnumerable<TVertex>>();
+            var vertexColor = new Dictionary<TVertex, int>();
+            var firstVertex = input.Graph.Vertices.First();
+
+
+            // Initialize remaining vertices as unassigned
+            foreach (var vertex in input.Graph.Vertices)
+            {
+                vertexColor[vertex] = -1; // no color is assigned to vertex
+            }
+
+            // Assign the first color to first vertex
+            vertexColor[firstVertex] = 0;
+
+            /*
+            A temporary array to store the available colors. True
+            value of available[cr] would mean that the color cr is
+            assigned to one of its adjacent vertices
+            */
+            bool[] available = new bool[V];
+            for (int usedColor = 0; usedColor < V; usedColor++)
+            {
+                available[usedColor] = false;
+            }
+
+            // Assign colors to remaining V-1 vertices            
+            foreach (var vertexOfGraph in input.Graph.Vertices)
+            {
+                if (!(vertexOfGraph.Equals(firstVertex)))
+                {
+                    // Process all adjacent vertices and flag their colors as unavailable
+                    foreach (var edgesOfProcessVertex in input.Graph.AdjacentEdges(vertexOfGraph))
+                    {
+                        if (vertexColor[edgesOfProcessVertex.GetOtherVertex(vertexOfGraph)] != -1)
+                        {
+                            available[vertexColor[edgesOfProcessVertex.GetOtherVertex(vertexOfGraph)]] = true;
+                        }
+                    }
+
+                    // Find the first available color
+                    int usedColor = new int();
+                    for (usedColor = 0; usedColor < V; usedColor++)
+                    {
+                        if (available[usedColor] == false)
+                            break;
+                    }
+
+                    // Assign the found color
+                    vertexColor[vertexOfGraph] = usedColor;
+
+                    // Reset the values back to false for the next iteration
+                    foreach (var edgesOfProcessVertex in input.Graph.AdjacentEdges(vertexOfGraph))
+                    {
+                        if (vertexColor[edgesOfProcessVertex.GetOtherVertex(vertexOfGraph)] != -1)
+                        {
+                            available[vertexColor[edgesOfProcessVertex.GetOtherVertex(vertexOfGraph)]] = false;
+                        }
+                    }
+                }
+            }
+            return new InputModel<TVertex, TEdge>
+            {
+                Graph = input.Graph,
+                Colors = vertexColor
+            };
+
+        }
+    }
+}
---
+++ tests/QuickGraph.Tests/Algorithms/VertexColoring/VertexColoringAlgorithmTest.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuickGraph.Algorithms.GraphColoring.VertexColoring;
+
+namespace QuickGraph.Tests.Algorithms.GraphColoring
+{
+    [TestClass]
+    public class VertexColoringAlgorithmTest
+    {
+        [TestMethod]
+        public void VertexColoringCompute()
+        {
+            /* 
+                                                 (1)
+                                                / | \ 
+            Generate undirected simle graph: (0)  |  (3)-(4)
+                                                \ | /
+                                                 (2)
+            */
+            var input = GenerateInput();
+            var grafWithColoredVertices = new VertexColoringAlgorithm<char, Edge<char>>(input).Compute();
+            var result = grafWithColoredVertices.Colors.Values.ToArray();
+
+            // Expecting to get 3 diferent colors
+            Assert.AreEqual(3, result.Max() + 1);
+
+            /* 
+            and corresponding colors of vertices:
+            0 vertex = 0 color
+            1 vertex = 1 color
+            2 vertex = 2 color
+            3 vertex = 0 color
+            4 vertex = 1 color
+            */
+            Assert.AreEqual(0, result[0]);
+            Assert.AreEqual(1, result[1]);
+            Assert.AreEqual(2, result[2]);
+            Assert.AreEqual(0, result[3]);
+            Assert.AreEqual(1, result[4]);
+        }
+
+        private InputModel<char, Edge<char>> GenerateInput()
+        {
+            var g = new UndirectedGraph<char, Edge<char>>(true);
+            var colorsOfVertex = new Dictionary<char, int>();
+
+
+            AddColoredVertex(g, colorsOfVertex, '0'); // 1 Vertex
+            AddColoredVertex(g, colorsOfVertex, '1'); // 2 Vertex
+            AddColoredVertex(g, colorsOfVertex, '2'); // 3 Vertex
+            AddColoredVertex(g, colorsOfVertex, '3'); // 4 Vertex
+            AddColoredVertex(g, colorsOfVertex, '4'); // 5 Vertex
+
+            AddEdgeBnColoredVertices(g, '0', '1'); // 1 Edge
+            AddEdgeBnColoredVertices(g, '0', '2'); // 2 Edge
+            AddEdgeBnColoredVertices(g, '1', '2'); // 3 Edge
+            AddEdgeBnColoredVertices(g, '1', '3'); // 4 Edge
+            AddEdgeBnColoredVertices(g, '2', '3'); // 5 Edge
+            AddEdgeBnColoredVertices(g, '3', '4'); // 6 Edge
+
+            return new InputModel<char, Edge<char>>
+            {
+                Graph = g,
+                Colors = colorsOfVertex
+            };
+        }
+        private void AddColoredVertex(
+               UndirectedGraph<char, Edge<char>> g,
+               Dictionary<char, int> colorsOfVertex,
+               char vertex)
+        {
+            var ac = vertex;
+            colorsOfVertex[ac] = 0;
+            g.AddVertex(ac);
+        }
+        private void AddEdgeBnColoredVertices(
+              UndirectedGraph<char, Edge<char>> g,
+              char source, char target)
+        {
+            var ac = new Edge<char>(source, target);
+            g.AddEdge(ac);
+        }
+
+    }
+}
