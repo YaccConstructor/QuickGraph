a928a1b5b338be1791fbc74263a6d2d3365a612a
Roman Fedorov <Roman2x@yandex.ru>
2016-04-10 00:24:33 +0300

    Refactor, avoid LINQ

--- src/QuickGraph/Algorithms/IsEulerianGraphAlgorithm.cs
+++ src/QuickGraph/Algorithms/IsEulerianGraphAlgorithm.cs
@@ -16,54 +16,62 @@ namespace QuickGraph.Algorithms
             this.graph = graph;
         }
 
+        private Tuple<int, int> firstAndSecondIndexOfTrue(bool[] data)
+        {
+            // if no true elements returns (-1, -1)
+            // if only one true element, returns (indexOfTrue, -1)
+            int firstIndex = -1, secondIndex = -1;
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i])
+                {
+                    if (firstIndex == -1)
+                    {
+                        firstIndex = i;
+                    }
+                    else
+                    {
+                        return new Tuple<int, int>(firstIndex, i);
+                    } 
+                }
+            }
+            return new Tuple<int, int>(firstIndex, secondIndex);
+        }
+
         public bool isEulerian()
         {
             var componentsAlgo = new ConnectedComponentsAlgorithm<TVertex, UndirectedEdge<TVertex>>(this.graph);
             componentsAlgo.Compute();
             
-            if (componentsAlgo.ComponentCount == 1)
+            // Only one component could contain edges
+            bool[] hasEdgesInComponent = new bool[componentsAlgo.ComponentCount];
+            foreach (var verticeAndComponent in componentsAlgo.Components)
             {
-                // Every vertice has even count of edges
-                foreach (var v in graph.Vertices)
+                if (graph.AdjacentEdges(verticeAndComponent.Key).Count() > 0) 
                 {
-                    if (graph.AdjacentEdges(v).Count() % 2 == 1)
+                    hasEdgesInComponent[verticeAndComponent.Value] = true;
+                }
+            }
+            var t = firstAndSecondIndexOfTrue(hasEdgesInComponent);
+            int firstIndex = t.Item1, secondIndex = t.Item2;
+            // More than one component contain edges
+            if (secondIndex != -1) 
+            {
+                return false;
+            }
+            else
+            {
+                // Now only one component contains edges, check is it an eulerian component
+                foreach (var verticeAndComponent in componentsAlgo.Components)
+                {   
+                    // Vertice in selected component and has even count of edges
+                    if (verticeAndComponent.Value == firstIndex && 
+                        graph.AdjacentEdges(verticeAndComponent.Key).Count() % 2 == 1)
                     {
                         return false;
                     }
                 }
                 return true;
-            }
-            else 
-            {
-                // Only one component could contain edges
-                bool[] hasEdgesInComponent = new bool[componentsAlgo.ComponentCount];
-                foreach (var verticeAndComponent in componentsAlgo.Components)
-                {
-                    if (graph.AdjacentEdges(verticeAndComponent.Key).Count() > 0) 
-                    {
-                        hasEdgesInComponent[verticeAndComponent.Value] = true;
-                    }
-                }
-                int firstIndex = Array.FindIndex(hasEdgesInComponent, x => x);
-                // More than one component contain edges
-                if (firstIndex != Array.FindLastIndex(hasEdgesInComponent, x => x)) 
-                {
-                    return false;
-                }
-                else
-                {
-                    // Now only one component contains edges, check is it an eulerian component
-                    foreach (var verticeAndComponent in componentsAlgo.Components)
-                    {   
-                        // Vertice in selected component and has even count of edges
-                        if (verticeAndComponent.Value == firstIndex && 
-                            graph.AdjacentEdges(verticeAndComponent.Key).Count() % 2 == 1)
-                        {
-                            return false;
-                        }
-                    }
-                    return true;
-                }
             }
         }
     }
