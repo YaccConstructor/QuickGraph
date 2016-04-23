ad883a5751da43a50bbb507ca36ca2515467e041
Eugene Auduchinok <eugene.auduchinok@gmail.com>
2016-04-24 02:04:02 +0300

    Move projects, cleanup

---
+++ src/GraphTask.MainForm/MainForm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using MainForm.Properties;
+
+namespace MainForm
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void algorithmPicker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Program.CurrentAlgorithm = Program.Algorithms[algorithmPicker.Text];
+            var algorithm = Program.CurrentAlgorithm;
+
+            algorithmInfoLabel.Text = algorithm.Description;
+            algorithmPlaybackGroupBox.Enabled = true;
+
+            algorithmOptionsGroupBox.Controls.Clear();
+            noOptionsLabel.Text = Resources.noOptionsAvailableText;
+            noOptionsLabel.Visible = algorithm.Options == null || algorithm.Options.Controls.Count == 0;
+
+            if (algorithm.Options == null) return;
+            foreach (Control control in algorithm.Options.Controls)
+            {
+                algorithmOptionsGroupBox.Controls.Add(control);
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            Program.CurrentAlgorithm.Run(editorField.Text);
+        }
+
+        private void editorNewButton_Click(object sender, EventArgs e)
+        {
+            editorField.Clear();
+            editorField.Focus();
+        }
+
+        private async void editorOpenButton_Click(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog
+            {
+                Filter = Resources.dotFileTypes,
+                Multiselect = false
+            };
+
+            if (dialog.ShowDialog() != DialogResult.OK) return;
+            try
+            {
+                editorField.Text = await new StreamReader(dialog.FileName).ReadToEndAsync();
+                editorField.Focus();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{Resources.couldNotReadFile}\n{ex.Message}");
+            }
+        }
+    }
+}
---
+++ src/GraphTask.PluginSample/PluginSample.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Common;
+using Mono.Addins;
+using QuickGraph;
+
+// This plugin demonstrates how to use plugin system.
+// Add your plugin reference to MainForm project.
+// Read more at Mono.Addins wiki page on GitHub.
+// https://github.com/mono/mono-addins/wiki/Architecture-Overview
+
+
+[assembly: Addin]
+[assembly: AddinDependency("GraphTasks", "1.0")]
+
+namespace PluginSample
+{
+    [Extension]
+    public class PluginSample : IAlgorithm
+    {
+        private readonly CheckBox _countSymbolsCheckBox = new CheckBox
+        {
+            Text = "Count symbols",
+            Location = new Point(12, 20)
+        };
+
+        public PluginSample()
+        {
+            Options.Controls.Add(_countSymbolsCheckBox);
+        }
+
+        public string Name => "Sample Plugin";
+        public string Author => "Eugene Auduchinok";
+
+        public string Description =>
+            "This plugin demonstrates how to use plugin system.\n" +
+            "Algorithms removes one vertex on each step until there's no vertices.\n";
+
+        public Panel Options { get; } = new Panel();
+        public Panel Output { get; } = new Panel();
+
+        public void Run(string dotSource)
+        {
+            var vertexFun = DotParserAdapter.VertexFunctions.Name;
+            var edgeFun = DotParserAdapter.EdgeFunctions<string>.VerticesOnly;
+
+            try
+            {
+                var graph = BidirectionalGraph<string, SEdge<string>>.LoadDot(dotSource, vertexFun, edgeFun);
+
+                var message = $"{graph.VertexCount} vertices.";
+                if (_countSymbolsCheckBox.Checked) message = $"{message} {dotSource.Length} symbols read.";
+                MessageBox.Show(message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
