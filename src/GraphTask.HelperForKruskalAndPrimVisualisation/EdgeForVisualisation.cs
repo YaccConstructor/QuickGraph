1831c7843ded24e66e5a6577704579bbd77f12ac
a.pykhtin@netrika.ru <AlIeN1q1w2e3r4>
2016-05-15 20:03:20 +0300

    Some code fix

---
+++ src/GraphTask.HelperForKruskalAndPrimVisualisation/EdgeForVisualisation.cs
@@ -0,0 +1,22 @@
+﻿using QuickGraph.GraphXAdapter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperForKruskalAndPrimVisualisation
+{
+    public class EdgeForVisualisation
+    {
+        public GraphXTaggedEdge<GraphXVertex, int> edge;
+        public bool isConteins;
+        public int number;
+        public EdgeForVisualisation(GraphXTaggedEdge<GraphXVertex, int> a, bool b, int c)
+        {
+            edge = a;
+            isConteins = b;
+            number = c;
+        }
+    }
+}
--- src/GraphTask.PrimVisualisation/Prim.cs
+++ src/GraphTask.PrimVisualisation/Prim.cs
@@ -32,40 +32,20 @@ namespace PrimVisualisation
     using Graph = BidirectionalGraph<GraphXVertex, GraphXTaggedEdge<GraphXVertex, int>>;
     using GraphArea = BidirectionalGraphArea<GraphXVertex, GraphXTaggedEdge<GraphXVertex, int>>;
     using Attributes = IDictionary<string, string>;
-
-    class EdgeForVisualisation
-    {
-        public GraphXTaggedEdge<GraphXVertex, int> edge;
-        public bool isConteins;
-        public int number;
-        public EdgeForVisualisation(GraphXTaggedEdge<GraphXVertex, int> a, bool b, int c)
-        {
-            edge = a;
-            isConteins = b;
-            number = c;
-        }
-    }
+    using HelperForKruskalAndPrimVisualisation;
 
     [Extension]
     public class PrimVisualisation : IAlgorithm
     {
         private readonly CheckBox _countSymbolsCheckBox;
-        private readonly GraphArea _graphArea;
-        private readonly GraphXZoomControl _zoomControl;
-        //private Stack<List<GraphSerializationData>> _steps;
-        private bool _hasStarted;
-        private bool _hasFinished;
         private List<EdgeForVisualisation> edges = new List<EdgeForVisualisation>();
-        private int i = -1;
+        private bool canBack;
+        private bool canFuther;
 
         public PrimVisualisation()
         {
             _countSymbolsCheckBox = new CheckBox { Text = "Count symbols when start", Location = new Point(12, 6) };
             Options.Controls.Add(_countSymbolsCheckBox);
-
-            _graphArea = new GraphArea();
-            _zoomControl = new GraphXZoomControl { Content = _graphArea };
-            Output.Controls.Add(new ElementHost { Dock = DockStyle.Fill, Child = _zoomControl });
         }
 
         public string Name => "Prim algorithm";
@@ -81,121 +61,47 @@ namespace PrimVisualisation
         public void Run(string dotSource)
         {
             edges.Clear();
-            i = -1;
-            if (_countSymbolsCheckBox.Checked)
-            {
-                MessageBox.Show($"Read {dotSource.Length} symbol(s).");
-            }
-
-            var vertexFun = VertexFactory.Name;
-            var edgeFun = EdgeFactory<GraphXVertex>.Weighted(0);
-
-            var graph = Graph.LoadDot(dotSource, vertexFun, edgeFun);
-            var grap = new UndirectedGraph<GraphXVertex, GraphXTaggedEdge<GraphXVertex, int>>();
-            foreach (var i in graph.Vertices)
-            {
-                if (!grap.ContainsVertex(i))
-                    grap.AddVertex(i);
-            }
-            foreach (var i in graph.Edges)
-            {
-                var z = grap.Edges.FirstOrDefault(x => (x.Source == i.Target) && (x.Target == i.Source));
-                if (grap.Edges.FirstOrDefault(x => (x.Source == i.Target) && (x.Target == i.Source)) == null)
-                {
-                    grap.AddEdge(i);
-                }
-            }
-
-            if (!graph.Vertices.Any())
-            {
-                MessageBox.Show("Graph is empty.");
-                return;
-            }
-            var prim = new QuickGraph.Algorithms.MinimumSpanningTree.PrimMinimumSpanningTreeAlgorithm<GraphXVertex, GraphXTaggedEdge<GraphXVertex, int>>(grap, x => x.Tag);
-            prim.TreeEdge += Kruskal_TreeEdge;
-            prim.ExamineEdge += Kruskal_ExamineEdge;
+            var graph = UndirectedGraphFactory.GetUnderectedGraphFromDot(dotSource);
+            var prim = new QuickGraph.Algorithms.MinimumSpanningTree.PrimMinimumSpanningTreeAlgorithm<GraphXVertex, GraphXTaggedEdge<GraphXVertex, int>>(graph, x => x.Tag);
+            prim.TreeEdge += Prim_TreeEdge;
+            prim.ExamineEdge += Prim_ExamineEdge;
             prim.Compute();
-            _graphArea.LogicCore.Graph = graph;
-            _graphArea.GenerateGraph();
-            _zoomControl.ZoomToFill();
-            foreach (var i in _graphArea.EdgesList)
-            {
-                i.Value.Foreground = new SolidColorBrush(Colors.WhiteSmoke);
-                i.Value.ShowLabel = true;
-                i.Value.ShowArrows = false;
-            }
-            _hasStarted = true;
-            _hasFinished = false;
+            HelperForKruskalAndPrimVisualisation.Run(dotSource, edges);
+            HelperForKruskalAndPrimVisualisation.CanBack += HelperForKruskalAndPrimVisualisation_CanBack;
+            HelperForKruskalAndPrimVisualisation.CanFuther += HelperForKruskalAndPrimVisualisation_CanFuther;
         }
 
-        private void Kruskal_ExamineEdge(GraphXTaggedEdge<GraphXVertex, int> e)
+        private void HelperForKruskalAndPrimVisualisation_CanFuther(bool x)
+        {
+            canFuther = x;
+        }
+
+        private void HelperForKruskalAndPrimVisualisation_CanBack(bool x)
+        {
+            canBack = x;
+        }
+
+        private void Prim_ExamineEdge(GraphXTaggedEdge<GraphXVertex, int> e)
         {
             edges.Add(new EdgeForVisualisation(e, false, edges.Count));
         }
 
-        private void Kruskal_TreeEdge(GraphXTaggedEdge<GraphXVertex, int> e)
+        private void Prim_TreeEdge(GraphXTaggedEdge<GraphXVertex, int> e)
         {
             edges.First(x => x.edge == e).isConteins = true;
         }
 
         public void NextStep()
         {
-            i++;
-            var edge = _graphArea.EdgesList.First(x => (x.Key.Source == edges[i].edge.Source) && (x.Key.Target == edges[i].edge.Target));
-            if (edges[i].isConteins)
-            {
-                var vertex1 = _graphArea.VertexList.First(x => x.Key == edge.Key.Source);
-                var vertex2 = _graphArea.VertexList.First(x => x.Key == edge.Key.Target);
-                vertex1.Value.Background = new SolidColorBrush(Colors.Aquamarine);
-                vertex2.Value.Background = new SolidColorBrush(Colors.Aquamarine);
-                edge.Value.Visibility = System.Windows.Visibility.Visible;
-                edge.Value.Foreground = new SolidColorBrush(Colors.Aquamarine);
-                edge.Value.ShowLabel = true;
-                edge.Value.ShowArrows = false;
-            }
-            else
-            {
-                edge.Value.Visibility = System.Windows.Visibility.Visible;
-                edge.Value.Foreground = new SolidColorBrush(Colors.BurlyWood);
-                edge.Value.ShowLabel = true;
-                edge.Value.ShowArrows = false;
-            }
-            if (!_graphArea.LogicCore.Graph.Vertices.Any())
-            {
-                _hasFinished = true;
-                return;
-            }
-            _zoomControl.ZoomToFill();
+            HelperForKruskalAndPrimVisualisation.NextStep();
         }
 
         public void PreviousStep()
         {
-            var edge = _graphArea.EdgesList.First(x => (x.Key.Source == edges[i].edge.Source) && (x.Key.Target == edges[i].edge.Target));
-            if (edges[i].isConteins)
-            {
-                var vertex1 = _graphArea.VertexList.First(x => x.Key == edge.Key.Source);
-                var vertex2 = _graphArea.VertexList.First(x => x.Key == edge.Key.Target);
-                var edge1 = edges.ToList().FirstOrDefault(x => x.edge.Source == vertex1.Key && x.isConteins && x.number < i)?.edge;
-                var edge2 = edges.ToList().FirstOrDefault(x => x.edge.Target == vertex1.Key && x.isConteins && x.number < i)?.edge;
-                var edge3 = edges.ToList().FirstOrDefault(x => x.edge.Source == vertex2.Key && x.isConteins && x.number < i)?.edge;
-                var edge4 = edges.ToList().FirstOrDefault(x => x.edge.Target == vertex2.Key && x.isConteins && x.number < i)?.edge;
-                if ((edge1 == null) && (edge2 == null))
-                    vertex1.Value.Background = new SolidColorBrush(Color.FromArgb(255, 227, 227, 227));
-                if ((edge3 == null) && (edge4 == null))
-                    vertex2.Value.Background = new SolidColorBrush(Color.FromArgb(255, 227, 227, 227));
-                edge.Value.Foreground = new SolidColorBrush(Colors.WhiteSmoke);
-            }
-            else
-            {
-                edge.Value.Foreground = new SolidColorBrush(Colors.WhiteSmoke);
-            }
-            i--;
-            _zoomControl.ZoomToFill();
-
-            _hasFinished = false;
+            HelperForKruskalAndPrimVisualisation.PreviousStep();
         }
 
-        public bool CanGoBack => i != -1;
-        public bool CanGoFurther => i != edges.Count - 1;
+        public bool CanGoBack => canBack;
+        public bool CanGoFurther => canFuther;
     }
 }
