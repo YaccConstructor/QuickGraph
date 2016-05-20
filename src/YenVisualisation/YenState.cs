8f709ceece58f8d58b86ac86e1bd1de45608f586
Vladislaff Evtushenko <starson4587@gmail.com>
2016-05-20 13:28:28 +0000

    - Yen algorithm visualysed

---
+++ src/YenVisualisation/YenState.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Media;
+
+namespace YenVisualisation
+{
+
+    public class YenState
+    {
+        public Brush Foreground { get; set; }
+        public bool ShowLabel { get; set; }
+        public bool ShowArrows { get; set; }
+    }
+
+}
