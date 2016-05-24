52a39ed2c4dedf745a2765df4d65121d92d831fb
Vladislaff Evtushenko <starson4587@gmail.com>
2016-05-24 09:35:04 +0000

    - Yen visualisation implemented

--- src/QuickGraph/Algorithms/ShortestPath/Yen/YenShortestPathsAlgorithm.cs
+++ src/QuickGraph/Algorithms/ShortestPath/Yen/YenShortestPathsAlgorithm.cs
@@ -4,133 +4,133 @@ using QuickGraph.Algorithms.Observers;
 
 namespace QuickGraph.Algorithms.ShortestPath.Yen
 {
-  public class YenShortestPathsAlgorithm<TVertex>
-  {
-    private TVertex sourceVertix;
-    private TVertex targetVertix;
-    // limit for amount of paths
-    private int k;
-    private AdjacencyGraph<TVertex, TaggedEquatableEdge<TVertex, double>> graph;
-
-    /*
-      double type of tag comes from Dijkstra’s algorithm,
-      which is used to get one shortest path.
-    */
-
-    public YenShortestPathsAlgorithm(AdjacencyGraph<TVertex, TaggedEquatableEdge<TVertex, double>> graph, TVertex s,
-      TVertex t, int k)
+    public class YenShortestPathsAlgorithm<TVertex>
     {
-      sourceVertix = s;
-      targetVertix = t;
-      this.k = k;
-      this.graph = graph;
-    }
+        private readonly TVertex _sourceVertix;
+        private readonly TVertex _targetVertix;
+        // limit for amount of paths
+        private int k;
+        private AdjacencyGraph<TVertex, TaggedEquatableEdge<TVertex, double>> _graph;
+        private readonly List<TaggedEquatableEdge<TVertex, double>> _removedEdges = new List<TaggedEquatableEdge<TVertex, double>>();
 
-    public IEnumerable<IEnumerable<TaggedEquatableEdge<TVertex, double>>> Execute()
-    {
-      var listShortestWays = new List<IEnumerable<TaggedEquatableEdge<TVertex, double>>>();
-      // find the first shortest way
-      var shortestWay = GetShortestPathInGraph(graph);
-      listShortestWays.Add(shortestWay);
+        /*
+         * for access from visualisation code
+         */
+        public IEnumerable<TaggedEquatableEdge<TVertex, double>> RemovedEdges
+          => _removedEdges;
 
-      /*
-       * in case of Dijkstra’s algorithm couldn't find any ways
-       */
-      if (shortestWay == null)
-      {
-        throw new InstanceNotFoundException();
-      }
+        /*
+          double type of tag comes from Dijkstra’s algorithm,
+          which is used to get one shortest path.
+        */
 
-      for (var i = 0; i < k - 1; i++)
-      {
-        var minDistance = double.MaxValue;
-        IEnumerable<TaggedEquatableEdge<TVertex, double>> pathSlot = null;
-        // slote for graph state without some edge
-        AdjacencyGraph<TVertex, TaggedEquatableEdge<TVertex, double>> graphSlot = null;
-        foreach (var edge in shortestWay)
+        public YenShortestPathsAlgorithm(AdjacencyGraph<TVertex, TaggedEquatableEdge<TVertex, double>> graph, TVertex s,
+          TVertex t, int k)
         {
-          // get new state without the edge
-          var newGraph = RemoveEdge(graph, edge);
-
-          //find shortest way in the new graph
-          var newPath = GetShortestPathInGraph(newGraph);
-          if (newPath == null)
-          {
-            continue;
-          }
-          var pathWeight = GetPathDistance(newPath);
-          if (pathWeight >= minDistance)
-          {
-            continue;
-          }
-          minDistance = pathWeight;
-          pathSlot = newPath;
-          graphSlot = newGraph;
+            _sourceVertix = s;
+            _targetVertix = t;
+            this.k = k;
+            this._graph = graph;
         }
-        if (pathSlot == null)
+
+        public IEnumerable<IEnumerable<TaggedEquatableEdge<TVertex, double>>> Execute()
         {
-          break;
+            var listShortestWays = new List<IEnumerable<TaggedEquatableEdge<TVertex, double>>>();
+            // find the first shortest way
+            var shortestWay = GetShortestPathInGraph(_graph);
+            listShortestWays.Add(shortestWay);
+
+            /*
+             * in case of Dijkstra’s algorithm couldn't find any ways
+             */
+            if (shortestWay == null)
+            {
+                throw new InstanceNotFoundException();
+            }
+
+            for (var i = 0; i < k - 1; i++)
+            {
+                var minDistance = double.MaxValue;
+                IEnumerable<TaggedEquatableEdge<TVertex, double>> pathSlot = null;
+                // slot for graph state without some edge
+                AdjacencyGraph<TVertex, TaggedEquatableEdge<TVertex, double>> graphSlot = null;
+                TaggedEquatableEdge<TVertex, double> removedEdge = null;
+                foreach (var edge in shortestWay)
+                {
+                    // get new state without the edge
+                    var newGraph = RemoveEdge(_graph, edge);
+
+                    //find shortest way in the new graph
+                    var newPath = GetShortestPathInGraph(newGraph);
+                    if (newPath == null)
+                    {
+                        continue;
+                    }
+                    var pathWeight = GetPathDistance(newPath);
+                    if (pathWeight >= minDistance)
+                    {
+                        continue;
+                    }
+                    minDistance = pathWeight;
+                    pathSlot = newPath;
+                    graphSlot = newGraph;
+                    removedEdge = edge;
+                }
+                if (pathSlot == null)
+                {
+                    break;
+                }
+                listShortestWays.Add(pathSlot);
+                _removedEdges.Add(removedEdge);
+                shortestWay = pathSlot;
+                _graph = graphSlot;
+            }
+            return listShortestWays;
         }
-        listShortestWays.Add(pathSlot);
-        shortestWay = pathSlot;
-        graph = graphSlot;
-      }
-      return listShortestWays;
-    }
 
-    private double GetPathDistance(IEnumerable<TaggedEquatableEdge<TVertex, double>> edges)
-    {
-      var pathSum = 0.0;
-      foreach (var edge in edges)
-      {
-        pathSum += edge.Tag;
-      }
-      return pathSum;
-    }
-
-    private IEnumerable<TaggedEquatableEdge<TVertex, double>> GetShortestPathInGraph(
-      AdjacencyGraph<TVertex, TaggedEquatableEdge<TVertex, double>> graph)
-    {
-      // calc distances beetween the start vertex and other
-      var dij = new DijkstraShortestPathAlgorithm<TVertex, TaggedEquatableEdge<TVertex, double>>(graph, e => e.Tag);
-      var vis = new VertexPredecessorRecorderObserver<TVertex, TaggedEquatableEdge<TVertex, double>>();
-      using (vis.Attach(dij))
-        dij.Compute(sourceVertix);
-
-      // get shortest path from start (source) vertex to target
-      IEnumerable<TaggedEquatableEdge<TVertex, double>> path;
-
-      return vis.TryGetPath(targetVertix, out path) ? path : null;
-    }
-
-    private AdjacencyGraph<TVertex, TaggedEquatableEdge<TVertex, double>> RemoveEdge(
-      AdjacencyGraph<TVertex, TaggedEquatableEdge<TVertex, double>> old,
-      TaggedEquatableEdge<TVertex, double> edgeRemoving)
-    {
-      // get copy of the grapth using Serialization and Deserialization
-      var copyGraph = old.Clone();
-
-      // remove the edge
-      foreach (var edge in copyGraph.Edges)
-      {
-        if (edge == edgeRemoving)
+        private double GetPathDistance(IEnumerable<TaggedEquatableEdge<TVertex, double>> edges)
         {
-          copyGraph.RemoveEdge(edge);
-          break;
+            var pathSum = 0.0;
+            foreach (var edge in edges)
+            {
+                pathSum += edge.Tag;
+            }
+            return pathSum;
         }
-      }
 
-      // get all edges but the removing one
-      var oldEdges = new List<TaggedEquatableEdge<TVertex, double>>();
-      foreach (var edge in old.Edges)
-      {
-        if (edge != edgeRemoving)
+        private IEnumerable<TaggedEquatableEdge<TVertex, double>> GetShortestPathInGraph(
+          AdjacencyGraph<TVertex, TaggedEquatableEdge<TVertex, double>> graph)
         {
-          oldEdges.Add(edge);
-        }
-      }
+            // calc distances beetween the start vertex and other
+            var dij = new DijkstraShortestPathAlgorithm<TVertex, TaggedEquatableEdge<TVertex, double>>(graph, e => e.Tag);
+            var vis = new VertexPredecessorRecorderObserver<TVertex, TaggedEquatableEdge<TVertex, double>>();
+            using (vis.Attach(dij))
+                dij.Compute(_sourceVertix);
 
-      return copyGraph;
+            // get shortest path from start (source) vertex to target
+            IEnumerable<TaggedEquatableEdge<TVertex, double>> path;
+
+            return vis.TryGetPath(_targetVertix, out path) ? path : null;
+        }
+
+        private AdjacencyGraph<TVertex, TaggedEquatableEdge<TVertex, double>> RemoveEdge(
+          AdjacencyGraph<TVertex, TaggedEquatableEdge<TVertex, double>> old,
+          TaggedEquatableEdge<TVertex, double> edgeRemoving)
+        {
+            // get copy of the grapth using Serialization and Deserialization
+            var copyGraph = old.Clone();
+
+            // remove the edge
+            foreach (var edge in copyGraph.Edges)
+            {
+                if (edge == edgeRemoving)
+                {
+                    copyGraph.RemoveEdge(edge);
+                    break;
+                }
+            }
+
+            return copyGraph;
+        }
     }
-  }
 }
---
+++ src/YenVisualisation/Yen.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+using System.Windows.Media;
+using Common;
+using GraphX.Controls;
+using Mono.Addins;
+using QuickGraph;
+using QuickGraph.Algorithms.ShortestPath.Yen;
+using QuickGraph.GraphXAdapter;
+
+[assembly: Addin]
+[assembly: AddinDependency("GraphTasks", "1.0")]
+namespace YenVisualisation
+{
+    using Graph = BidirectionalGraph<GraphXVertex, GraphXTaggedEdge<GraphXVertex, int>>;
+    using GraphArea = BidirectionalGraphArea<GraphXVertex, GraphXTaggedEdge<GraphXVertex, int>>;
+
+    [Extension]
+    public class Yen : IAlgorithm
+    {
+        private GraphArea _graphArea;
+        private readonly GraphXZoomControl _zoomControl;
+
+        private readonly TextBox _k;
+        private readonly TextBox _startVertex;
+        private readonly TextBox _finishVertex;
+        private readonly CheckBox _showMessages;
+
+        private List<IEnumerable<TaggedEquatableEdge<char, double>>> _paths;
+        private List<TaggedEquatableEdge<char, double>> _removedEdges;
+        private int _currentIteration;
+        private Dictionary<int, List<YenState>> _states;
+
+        public Yen()
+        {
+            _k = new TextBox { Text = "100", Location = new Point(12, 6) };
+            _startVertex = new TextBox { Location = new Point(12, 30) /*, Text = "1"*/ };
+            _finishVertex = new TextBox { Location = new Point(12, 54)/*, Text = "5"*/ };
+            _showMessages = new CheckBox { Location = new Point(12, 74), Checked = true };
+            var kLabel = new Label { Location = new Point(120, 6), Text = "k" };
+            var sLabel = new Label { Location = new Point(120, 30), Text = "start vertex" };
+            var fLabel = new Label { Location = new Point(120, 54), Text = "target vertex" };
+            var smLabel = new Label { Location = new Point(120, 74), Text = "messages" };
+
+            Options.Controls.AddRange(new Control[] { _k, _startVertex, _finishVertex, kLabel, sLabel, fLabel, _showMessages, smLabel });
+
+            _graphArea = new GraphArea();
+            _zoomControl = new GraphXZoomControl { Content = _graphArea };
+            Output.Controls.Add(new ElementHost { Dock = DockStyle.Fill, Child = _zoomControl });
+
+            _states = new Dictionary<int, List<YenState>>();
+        }
+
+        private void GetYenIterations(Graph graph)
+        {
+            var yenGraph = ToYenInput(graph);
+            var yen = new YenShortestPathsAlgorithm<char>(yenGraph, _startVertex.Text.First(), _finishVertex.Text.First(), int.Parse(_k.Text));
+            _paths = yen.Execute().ToList();
+            _removedEdges = yen.RemovedEdges.ToList();
+        }
+
+        private void EnlightBeginEndVertix()
+        {
+            _graphArea.VertexList.First(x => x.Key.Text == _startVertex.Text).Value.Background = new SolidColorBrush(Colors.BurlyWood);
+            _graphArea.VertexList.First(x => x.Key.Text == _finishVertex.Text).Value.Background = new SolidColorBrush(Colors.DarkSeaGreen);
+        }
+
+        // From Yen to Vis
+        private KeyValuePair<GraphXTaggedEdge<GraphXVertex, int>, EdgeControl> FindSameEdge(TaggedEquatableEdge<char, double> edge)
+          => _graphArea.EdgesList.First(x => x.Key.Source.Text == edge.Source.ToString() && x.Key.Target.Text == edge.Target.ToString()
+          || x.Key.Source.Text == edge.Target.ToString() && x.Key.Source.Text == edge.Target.ToString());
+
+        public void PreviousStep()
+        {
+            ApplyState(--_currentIteration);
+        }
+
+        private void ApplyState(int key)
+        {
+            var states = _states[key];
+            int index = 0;
+            foreach (var edge in _graphArea.EdgesList.Values)
+            {
+                edge.Foreground = states[index].Foreground;
+                edge.ShowArrows = states[index].ShowArrows;
+                edge.ShowLabel = states[index].ShowLabel;
+                index++;
+            }
+        }
+
+        private void SaveState(int key)
+        {
+            if (!_states.ContainsKey(key))
+            {
+                _states[key] = new List<YenState>();
+                foreach (var edge in _graphArea.EdgesList.Values)
+                {
+                    _states[key].Add(new YenState
+                    {
+                        Foreground = edge.Foreground,
+                        ShowArrows = edge.ShowArrows,
+                        ShowLabel = edge.ShowLabel
+                    });
+                }
+            }
+        }
+
+        public void NextStep()
+        {
+            var index = _currentIteration / 3;
+            SaveState(_currentIteration);
+            switch (_currentIteration % 3)
+            {
+                case 0:
+                    foreach (var edge in _paths[index])
+                    {
+                        FindSameEdge(edge).Value.Foreground = new SolidColorBrush(Colors.Red);
+                    }
+                    if (_showMessages.Checked)
+                    {
+                        MessageBox.Show("Found the shortest way![red]\nIt will be the current shortest way\n");
+                    }
+                    break;
+                case 1:
+                    FindSameEdge(_removedEdges[index]).Value.Foreground = new SolidColorBrush(Colors.Yellow);
+                    if (_showMessages.Checked)
+                    {
+                        MessageBox.Show("We have analysed all edges of the current shortest way!\n We want to pick one of this edges for excluding it from" +
+                                        " the graph. If we deleted the yellow one, it would give us the new best shortest path. If we deleted one of rest red" +
+                                        " edges it give us longer way or even no way to the target vertex.");
+                    }
+                    break;
+                default:
+                    foreach (var edge in _paths[index])
+                    {
+                        FindSameEdge(edge).Value.Foreground = new SolidColorBrush(Colors.Black);
+                    }
+                    FindSameEdge(_removedEdges[index]).Value.Foreground = new SolidColorBrush(Colors.White);
+                    FindSameEdge(_removedEdges[index]).Value.ShowLabel = false;
+                    FindSameEdge(_removedEdges[index]).Value.ShowArrows = false;
+                    if (_showMessages.Checked)
+                    {
+                        MessageBox.Show("The yellow edge is removed from our graph!\n");
+                    }
+                    break;
+            }
+            _currentIteration++;
+        }
+
+        public void Run(string dotSource)
+        {
+            if (_startVertex.Text.Length == 0 || _finishVertex.Text.Length == 0)
+            {
+                MessageBox.Show("Please specify start and finish vertices first!\n");
+                return;
+            }
+
+            var graph = Graph.LoadDot(dotSource, VertexFactory.Name, EdgeFactory<GraphXVertex>.Weighted(0));
+            try
+            {
+                GetYenIterations(graph);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Sorry! \n Yen algorithm doesn't supporst this graph! \n");
+            }
+
+            _currentIteration = 0;
+
+            _graphArea.LogicCore.Graph = graph;
+            _graphArea.GenerateGraph();
+            _zoomControl.ZoomToFill();
+
+            ShowWeights();
+            EnlightBeginEndVertix();
+        }
+
+        private void ShowWeights()
+        {
+            foreach (var i in _graphArea.EdgesList)
+            {
+                i.Value.ShowLabel = true;
+            }
+        }
+
+        public string Name =>
+          "Yen algorithm";
+        public string Author =>
+          "Vlad Evtushenko";
+
+        public string Description =>
+          "Find k shortest ways in directed graph \n";
+
+        public Panel Options { get; } =
+          new Panel { Dock = DockStyle.Fill };
+        public Panel Output { get; } =
+          new Panel { Dock = DockStyle.Fill };
+
+        public bool CanGoBack =>
+          _currentIteration > 0;
+
+        public bool CanGoFurther =>
+            _paths != null && _currentIteration < (_paths.Count - 1) * 3 + 1;
+
+        private AdjacencyGraph<char, TaggedEquatableEdge<char, double>> ToYenInput(Graph graph)
+        {
+            var formatedGraph = new AdjacencyGraph<char, TaggedEquatableEdge<char, double>>(true);
+            formatedGraph.AddVertexRange(graph.Vertices.Select(x => x.Text.First()));
+            formatedGraph.AddEdgeRange(
+              graph.Edges.Select(
+                x => new TaggedEquatableEdge<char, double>(x.Source.Text.First(), x.Target.Text.First(), x.Tag)));
+            return formatedGraph;
+        }
+    }
+}
