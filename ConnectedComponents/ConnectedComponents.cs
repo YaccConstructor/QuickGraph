9a00343ae61ec92d5de461989bf5c976b2fba1da
KseniyaGonta <gontakseniya@gmail.com>
2016-06-11 20:39:44 +0300

    Added connected components visualisation

---
+++ ConnectedComponents/ConnectedComponents.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+using System.Windows.Media;
+using Mono.Addins;
+using QuickGraph.GraphXAdapter;
+using Point = System.Drawing.Point;
+
+[assembly: Addin]
+[assembly: AddinDependency("GraphTasks", "1.0")]
+
+namespace ConnectedComponents
+{
+    using GraphArea = BidirectionalGraphArea<GraphXVertex, GraphXTaggedEdge<GraphXVertex, int>>;
+    abstract public class ConnectedComponents //: IAlgorithm
+    {
+        // public ConnectedComponents()
+        //{ }
+        protected int steps;
+        protected int step;
+        protected int componentNum;
+        protected List<int> diffBySteps;
+        protected readonly GraphArea graphArea = new GraphArea();
+        protected List<GraphXVertex> vertices;
+        protected readonly CheckBox _countSymbolsCheckBox;
+        protected readonly GraphXZoomControl _zoomControl;
+
+        public ConnectedComponents()
+        {
+            _countSymbolsCheckBox = new CheckBox { Text = "Count symbols when start", Location = new Point(12, 6) };
+            Options.Controls.Add(_countSymbolsCheckBox);
+
+            _zoomControl = new GraphXZoomControl { Content = graphArea };
+            Output.Controls.Add(new ElementHost { Dock = DockStyle.Fill, Child = _zoomControl });
+        }
+
+        public Panel Options { get; } = new Panel { Dock = DockStyle.Fill };
+        public Panel Output { get; } = new Panel { Dock = DockStyle.Fill };
+
+
+        public void NextStep()
+        {
+            step++;
+            HighlightNextStep();
+            if (!graphArea.LogicCore.Graph.Vertices.Any())
+            {
+                return;
+            }
+        }
+
+        public void PreviousStep()
+        {
+            HighlightPrevStep();
+            step--;
+        }
+
+        private void HighlightNextStep()
+        {
+            foreach (KeyValuePair<GraphXVertex, GraphX.Controls.VertexControl> pair in graphArea.VertexList)
+            {
+                if (pair.Key.Text == vertices.ElementAt(step).Text)
+                {
+                    switch (diffBySteps.ElementAt(step))
+                    {
+                        case 1:
+                            pair.Value.Background = new SolidColorBrush(Colors.Green); break;
+                        case 2:
+                            pair.Value.Background = new SolidColorBrush(Colors.Red); break;
+                        case 3:
+                            pair.Value.Background = new SolidColorBrush(Colors.Yellow); break;
+                        case 4:
+                            pair.Value.Background = new SolidColorBrush(Colors.Pink); break;
+                        case 5:
+                            pair.Value.Background = new SolidColorBrush(Colors.PowderBlue); break;
+                        case 6:
+                            pair.Value.Background = new SolidColorBrush(Colors.RosyBrown); break;
+                        case 7:
+                            pair.Value.Background = new SolidColorBrush(Colors.Salmon); break;
+                        case 0:
+                            pair.Value.Background = new SolidColorBrush(Colors.Sienna); break;
+                    }
+                }
+            }
+        }
+
+        private void HighlightPrevStep()
+        {
+            foreach (KeyValuePair<GraphXVertex, GraphX.Controls.VertexControl> pair in graphArea.VertexList)
+            {
+                if (pair.Key.Text == vertices.ElementAt(step).Text)
+                {
+                    pair.Value.Background = new SolidColorBrush(Colors.LightGray);
+                }
+            }
+
+        }
+        public bool CanGoBack => step >= 0;
+        public bool CanGoFurther => step < steps - 1;
+    }
+}
--- src/QuickGraph/Algorithms/ConnectedComponents/StronglyConnectedComponentAlgorithm.cs
+++ src/QuickGraph/Algorithms/ConnectedComponents/StronglyConnectedComponentAlgorithm.cs
@@ -13,20 +13,24 @@ namespace QuickGraph.Algorithms.ConnectedComponents
 #endif
     public sealed class StronglyConnectedComponentsAlgorithm<TVertex, TEdge> :
         AlgorithmBase<IVertexListGraph<TVertex, TEdge>>,
-        IConnectedComponentAlgorithm<TVertex,TEdge,IVertexListGraph<TVertex, TEdge>>
+        IConnectedComponentAlgorithm<TVertex, TEdge, IVertexListGraph<TVertex, TEdge>>
         where TEdge : IEdge<TVertex>
     {
-		private readonly IDictionary<TVertex,int> components;
-		private readonly Dictionary<TVertex,int> discoverTimes;
-		private readonly Dictionary<TVertex,TVertex> roots;
-		private Stack<TVertex> stack;
-		int componentCount;
-		int dfsTime;
+        private readonly IDictionary<TVertex, int> components;
+        private readonly Dictionary<TVertex, int> discoverTimes;
+        private readonly Dictionary<TVertex, TVertex> roots;
+        private Stack<TVertex> stack;
+        int componentCount;
+        int dfsTime;
+        private List<int> diffBySteps;
+        private int step;
+        private List<TVertex> vertices;
+        List<BidirectionalGraph<TVertex, TEdge>> graphs;
 
         public StronglyConnectedComponentsAlgorithm(
-            IVertexListGraph<TVertex,TEdge> g)
-            :this(g, new Dictionary<TVertex,int>())
-		{}
+            IVertexListGraph<TVertex, TEdge> g)
+            : this(g, new Dictionary<TVertex, int>())
+        { }
 
         public StronglyConnectedComponentsAlgorithm(
             IVertexListGraph<TVertex, TEdge> g,
@@ -36,122 +40,188 @@ namespace QuickGraph.Algorithms.ConnectedComponents
 
         public StronglyConnectedComponentsAlgorithm(
             IAlgorithmComponent host,
-            IVertexListGraph<TVertex,TEdge> g,
-			IDictionary<TVertex,int> components)
-            :base(host, g)
-		{
+            IVertexListGraph<TVertex, TEdge> g,
+            IDictionary<TVertex, int> components)
+            : base(host, g)
+        {
             Contract.Requires(components != null);
 
-			this.components = components;
+            this.components = components;
             this.roots = new Dictionary<TVertex, TVertex>();
             this.discoverTimes = new Dictionary<TVertex, int>();
             this.stack = new Stack<TVertex>();
-			this.componentCount = 0;
-			this.dfsTime = 0;
+            this.componentCount = 0;
+            this.dfsTime = 0;
         }
 
-		public IDictionary<TVertex,int> Components
-		{
-			get
-			{
-				return this.components;
-			}
-		}
+        public IDictionary<TVertex, int> Components
+        {
+            get
+            {
+                return this.components;
+            }
+        }
 
-		public IDictionary<TVertex,TVertex> Roots
-		{
-			get
-			{
-				return this.roots;
-			}
-		}
+        public IDictionary<TVertex, TVertex> Roots
+        {
+            get
+            {
+                return this.roots;
+            }
+        }
 
         public IDictionary<TVertex, int> DiscoverTimes
         {
-			get
-			{
-				return this.discoverTimes;
-			}
-		}
+            get
+            {
+                return this.discoverTimes;
+            }
+        }
 
-		public int ComponentCount
-		{
-			get
-			{
-				return this.componentCount;
-			}
-		}
+        public int ComponentCount
+        {
+            get
+            {
+                return this.componentCount;
+            }
+        }
 
-		private void DiscoverVertex(TVertex v)
-		{
-			this.Roots[v]=v;
-			this.Components[v]=int.MaxValue;
-			this.DiscoverTimes[v]=dfsTime++;
-			this.stack.Push(v);
-		}
+        public List<TVertex> Vertices
+        {
+            get
+            {
+                return this.vertices;
+            }
+        }
 
-		/// <summary>
-		/// Used internally
-		/// </summary>
-		private void FinishVertex(TVertex v)
-		{
+        public int Steps
+        {
+            get
+            {
+                return step;
+            }
+        }
+        public List<int> DiffBySteps
+        {
+            get
+            {
+                return diffBySteps;
+            }
+        }
+
+        private void DiscoverVertex(TVertex v)
+        {
+            this.Roots[v] = v;
+            this.Components[v] = int.MaxValue;
+
+            // this.diffBySteps[step] = componentCount;
+            this.diffBySteps.Add(componentCount);
+            this.vertices.Add(v);
+            this.step++;
+
+            this.DiscoverTimes[v] = dfsTime++;
+            this.stack.Push(v);
+        }
+
+        private void FinishVertex(TVertex v)
+        {
             var roots = this.Roots;
 
-			foreach(var e in this.VisitedGraph.OutEdges(v))
-			{
-				var w = e.Target;
-				if (this.Components[w] == int.MaxValue)
-					roots[v] = this.MinDiscoverTime(roots[v], roots[w]);
-			}
+            foreach (var e in this.VisitedGraph.OutEdges(v))
+            {
+                var w = e.Target;
+                if (this.Components[w] == int.MaxValue)
+                    roots[v] = this.MinDiscoverTime(roots[v], roots[w]);
+            }
 
-			if (this.roots[v].Equals(v)) 
-			{
-				var w = default(TVertex);
-				do 
-				{
-					w = this.stack.Pop(); 
-					this.Components[w] = componentCount;
-				} 
-				while (!w.Equals(v));
-				++componentCount;
-			}	
-		}
+            if (this.roots[v].Equals(v))
+            {
+                var w = default(TVertex);
+                do
+                {
+                    w = this.stack.Pop();
+                    this.Components[w] = componentCount;
 
-		private TVertex MinDiscoverTime(TVertex u, TVertex v)
-		{
+
+                    this.diffBySteps.Add(componentCount);
+                    this.vertices.Add(w);
+                    this.step++;
+                }
+                while (!w.Equals(v));
+                ++componentCount;
+
+            }
+        }
+
+        private TVertex MinDiscoverTime(TVertex u, TVertex v)
+        {
             Contract.Requires(u != null);
             Contract.Requires(v != null);
-            Contract.Ensures(this.DiscoverTimes[u] < this.DiscoverTimes[v] 
-                ? Contract.Result<TVertex>().Equals(u) 
+            Contract.Ensures(this.DiscoverTimes[u] < this.DiscoverTimes[v]
+                ? Contract.Result<TVertex>().Equals(u)
                 : Contract.Result<TVertex>().Equals(v)
                 );
 
-			if (this.discoverTimes[u] < this.discoverTimes[v])
-				return u;
-			else
-				return v;
-		}
+            if (this.discoverTimes[u] < this.discoverTimes[v])
+                return u;
+            else
+                return v;
+        }
 
-		protected override void InternalCompute()
-		{
+        public List<BidirectionalGraph<TVertex, TEdge>> Graphs
+        {
+            get
+            {
+                int i;
+                graphs = new List<BidirectionalGraph<TVertex, TEdge>>(componentCount + 1);
+                for (i = 0; i < componentCount; i++)
+                {
+                    graphs.Add(new BidirectionalGraph<TVertex, TEdge>());
+                }
+                foreach (TVertex componentName in components.Keys)
+                {
+                    graphs[components[componentName]].AddVertex(componentName);
+                }
+
+                foreach (TVertex vertex in VisitedGraph.Vertices)
+                {
+                    foreach (TEdge edge in VisitedGraph.OutEdges(vertex))
+                    {
+
+                        if (components[vertex] == components[edge.Target])
+                        {
+                            graphs[components[vertex]].AddEdge(edge);
+                        }
+                    }
+                }
+                return graphs;
+            }
+
+        }
+
+        protected override void InternalCompute()
+        {
             Contract.Ensures(this.ComponentCount >= 0);
-            Contract.Ensures(this.VisitedGraph.VertexCount == 0 || this.ComponentCount > 0); 
+            Contract.Ensures(this.VisitedGraph.VertexCount == 0 || this.ComponentCount > 0);
             Contract.Ensures(Enumerable.All(this.VisitedGraph.Vertices, v => this.Components.ContainsKey(v)));
             Contract.Ensures(this.VisitedGraph.VertexCount == this.Components.Count);
             Contract.Ensures(Enumerable.All(this.Components.Values, c => c <= this.ComponentCount));
 
-			this.Components.Clear();
-			this.Roots.Clear();
-			this.DiscoverTimes.Clear();
+            diffBySteps = new List<int>();
+            vertices = new List<TVertex>();
+
+            this.Components.Clear();
+            this.Roots.Clear();
+            this.DiscoverTimes.Clear();
             this.stack.Clear();
-			this.componentCount = 0;
-			this.dfsTime = 0;
+            this.componentCount = 0;
+            this.dfsTime = 0;
 
             DepthFirstSearchAlgorithm<TVertex, TEdge> dfs = null;
             try
             {
                 dfs = new DepthFirstSearchAlgorithm<TVertex, TEdge>(
-                    this, 
+                    this,
                     VisitedGraph,
                     new Dictionary<TVertex, GraphColor>(this.VisitedGraph.VertexCount)
                     );
@@ -168,6 +238,6 @@ namespace QuickGraph.Algorithms.ConnectedComponents
                     dfs.FinishVertex -= new VertexAction<TVertex>(this.FinishVertex);
                 }
             }
-		}
+        }
     }
 }
