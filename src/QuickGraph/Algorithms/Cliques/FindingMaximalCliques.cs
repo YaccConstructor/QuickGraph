38e4db2b6678681d27ff7f4f18c2ac4602a790e3
Nik <borovikow.nikita@gmail.com>
2016-09-25 23:03:31 +0300

    Algorithm realization
    Add algorithm without connection to QuickGraph's classes

---
+++ src/QuickGraph/Algorithms/Cliques/FindingMaximalCliques.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickGraph.Algorithms.Cliques
+{
+    public class Graph
+    {
+        public List<Edge> Edges;
+        public List<char> Vertexes;
+    }
+    public class Edge
+    {
+        public Edge(char _v1, char _v2)
+        {
+            v1 = _v1;
+            v2 = _v2;
+        }
+        public char v1;
+        public char v2;
+    }
+
+    //class Vertex
+    //{
+    //    public List<Vertex> neighbors;
+    //}
+
+    public class FindingMaximalCliques
+    {
+        public Dictionary<char, List<char>> neighbors = new Dictionary<char, List<char>>();
+        private int amountVertices;
+        private int amountEdges;
+        public Graph graph;
+        public List<List<char>> cliques = new List<List<char>>();
+
+        public FindingMaximalCliques(Graph g)
+        {
+            graph = g;
+            amountEdges = graph.Edges.Count;
+            amountVertices = graph.Vertexes.Count;
+            
+        }
+        public void FindNeighbors()
+        {
+            for (int i = 0; i < amountVertices; i++)
+            {
+                neighbors.Add(graph.Vertexes[i], new List<char>());
+            }
+
+            for (int i = 0; i < amountEdges; i++)
+            {
+                var edge = graph.Edges[i];
+                neighbors[edge.v1].Add(edge.v2);
+                neighbors[edge.v2].Add(edge.v1);
+            }
+            
+        }
+        
+        public void Run()
+        {
+            var R = new List<char>();
+            var P = new List<char>(graph.Vertexes);
+            var X = new List<char>();
+            Compute(R, P, X);
+        }
+
+        public void Compute(List<char> R, List<char> P, List<char> X)
+        {
+            if (!P.Any() && !X.Any()) // Тут или?
+            {
+                cliques.Add(new List<char>(R));
+            }
+
+            for (int i = 0; i < P.Count; i++)
+            {
+                var v = P[i];
+                R.Add(v);
+                Compute(R, P.Intersect(neighbors[v]).ToList(), X.Intersect(neighbors[v]).ToList() );
+                R.Remove(v);
+                P.Remove(v);
+                X.Add(v);
+            }
+        }
+    }
+}
---
+++ tests/QuickGraph.Tests/Algorithms/Cliques/FindingMaximalCliquesTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuickGraph.Algorithms.Cliques;
+
+namespace QuickGraph.Tests.Algorithms.Cliques
+{
+    [TestClass]
+    public class FindingMaximalCliquesTest
+    {
+        private Graph GenerateGraph()
+        {
+            var graph = new Graph();
+            graph.Edges = new List<Edge>();
+            graph.Vertexes = new List<char>();
+            graph.Vertexes.AddRange(new[] { 'a', 'b', 'c', 'd', 'e' });
+            graph.Edges.Add(new Edge('a', 'b'));
+            graph.Edges.Add(new Edge('a', 'c'));
+            graph.Edges.Add(new Edge('b', 'c'));
+            graph.Edges.Add(new Edge('b', 'd'));
+            graph.Edges.Add(new Edge('c', 'd'));
+            graph.Edges.Add(new Edge('d', 'e'));
+            return graph;
+        }
+
+        [TestMethod]
+        public void TestFindNeighbors()
+        {
+            var graph = GenerateGraph();
+            FindingMaximalCliques fmc = new FindingMaximalCliques(graph);
+
+            fmc.FindNeighbors();
+
+            var neighbors = fmc.neighbors;
+            Assert.AreEqual(neighbors['a'].ToArray().SequenceEqual(new [] { 'b', 'c'}), true);
+            Assert.AreEqual(neighbors['b'].ToArray().SequenceEqual(new [] { 'a', 'c', 'd' }), true);
+            Assert.AreEqual(neighbors['c'].ToArray().SequenceEqual(new [] { 'a', 'b', 'd' }), true);
+            Assert.AreEqual(neighbors['d'].ToArray().SequenceEqual(new [] { 'b', 'c', 'e' }), true);
+            Assert.AreEqual(neighbors['e'].ToArray().SequenceEqual(new [] { 'd' }), true);
+        }
+
+        [TestMethod]
+        public void TestFindCliques()
+        {
+            var graph = GenerateGraph();
+            FindingMaximalCliques fmc = new FindingMaximalCliques(graph);
+            fmc.FindNeighbors();
+            fmc.Run();
+            var cliques = fmc.cliques;
+            Assert.AreEqual(cliques.ElementAt(0).ToArray().SequenceEqual(new[] { 'a', 'b', 'c' }), true);
+            Assert.AreEqual(cliques.ElementAt(0).ToArray().SequenceEqual(new[] { 'c', 'b', 'd' }), true);
+            Assert.AreEqual(cliques.ElementAt(0).ToArray().SequenceEqual(new[] { 'e', 'd' }), true);
+
+        }
+    }
+}
